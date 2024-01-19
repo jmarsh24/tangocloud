240ca6b9cdd94ab712339abe121a64ad9294490c
Justin Marsh <jmarsh24@gmail.com>
2024-01-19 01:52:47 +0100

    add extensions

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -10,10 +10,10 @@ echo "Installing dependencies..."
 bundle install
 yarn install
 
-echo "Copying database.yml..."
-cp -n config/database.yml.example config/database.yml
+# echo "Copying database.yml..."
+# cp -n config/database.yml.example config/database.yml
 
 echo "Creating database..."
-bin/rails db:prepare 
+bin/rails db:prepare
 
 echo "Done!"
