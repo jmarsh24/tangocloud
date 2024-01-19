2e887c54d522509344a4f25d40a53f42dc9740b8
Justin Marsh <jmarsh24@gmail.com>
2024-01-19 15:58:22 +0100

    rubocop

--- db/migrate/20240117194927_create_album_audio_transfers.rb
+++ db/migrate/20240117194927_create_album_audio_transfers.rb
@@ -3,7 +3,7 @@
 class CreateAlbumAudioTransfers < ActiveRecord::Migration[7.1]
   def change
     create_table :album_audio_transfers, force: true, id: false do |t|
-        t.primary_key :id, :string, default: -> { "ULID()" }
+      t.primary_key :id, :string, default: -> { "ULID()" }
       t.belongs_to :album, null: false, foreign_key: true, type: :string
       t.belongs_to :audio_transfer, null: false, foreign_key: true, type: :string
       t.integer :position, null: false, default: 0
