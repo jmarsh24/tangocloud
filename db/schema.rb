0aed8901f8f4807a0119ad8a316596add8c59ee0
Justin Marsh <jmarsh24@gmail.com>
2024-01-19 19:31:33 +0100

    add solid cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_17_194927) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_19_183033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "btree_gist"
@@ -333,6 +333,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_17_194927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["slug"], name: "index_singers_on_slug"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_solid_cache_entries_on_key", unique: true
   end
 
   create_table "subscriptions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
