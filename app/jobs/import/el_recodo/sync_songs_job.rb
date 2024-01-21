19e820b9707d6f7eb656af152e2f812afb3a3a4b
Justin Marsh <jmarsh24@gmail.com>
2024-01-21 16:45:44 +0100

    wip

---
+++ app/jobs/import/el_recodo/sync_songs_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Import::ElRecodo::SyncSongsJob < ApplicationJob
+  include GoodJob::ActiveJobExtensions::Concurrency
+  queue_as :background_sync
+  good_job_control_concurrency_with(perform_limit: 1)
+
+  def perform
+    ::Import::ElRecodo::SongSynchronizer.new.sync_songs
+  end
+end
