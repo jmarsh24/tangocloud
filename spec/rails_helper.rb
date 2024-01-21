91547631aeb6b5214720d1af10e3a8920d9eb9b4
Justin Marsh <jmarsh24@gmail.com>
2024-01-21 18:20:42 +0100

    fix test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ RSpec.configure do |config|
   config.include ActiveSupport::Testing::TimeHelpers
   config.include ActiveJob::TestHelper
 
-  config.fixture_path = ["#{::Rails.root}/spec/fixtures"]
+  config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
   config.use_transactional_fixtures = true
   config.global_fixtures = :all
   config.infer_spec_type_from_file_location!
