4a33032f76108db23fe6b96fae2836e1fc6310a7
Justin Marsh <jmarsh24@gmail.com>
2024-01-21 01:47:56 +0100

    fix mistake

--- db/migrate/20240114234956_create_tandas.rb
+++ db/migrate/20240114234956_create_tandas.rb
@@ -3,7 +3,7 @@
 class CreateTandas < ActiveRecord::Migration[7.1]
   def change
     create_table :tandas, id: :uuid do |t|
-      t.string :name, null: false, default: ""
+      t.string :name, null: false
       t.string :description
       t.boolean :public, null: false, default: true
       t.belongs_to :audio_transfer, null: false, foreign_key: true, type: :uuid
