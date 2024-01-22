d38c849c76b366352f899054c8cfaf702fc020fd
Justin Marsh <jmarsh24@gmail.com>
2024-01-22 12:41:24 +0100

    namespace middleware

--- lib/middleware/silent_logger_middleware.rb
+++ lib/middleware/silent_logger_middleware.rb
@@ -2,12 +2,14 @@
 
 require "rails/rack/logger"
 
-class SilentLoggerMiddleware < Rails::Rack::Logger
-  def call(env)
-    if env["REQUEST_METHOD"] == "GET" && env["PATH_INFO"] == "/up"
-      Rails.logger.silence { super }
-    else
-      super
+module Middleware
+  class SilentLoggerMiddleware < Rails::Rack::Logger
+    def call(env)
+      if env["REQUEST_METHOD"] == "GET" && env["PATH_INFO"] == "/up"
+        Rails.logger.silence { super }
+      else
+        super
+      end
     end
   end
 end
