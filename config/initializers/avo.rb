82a4cd0e29ffa59864d5733cb29f9083ea29c318
Justin Marsh <jmarsh24@gmail.com>
2024-01-24 19:26:42 +0100

    update csv export

---
+++ config/initializers/avo.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Avo.configure do |config|
+  config.current_user_method = :current_user
+  config.per_page_steps = [48, 96, 256, 512, 1024]
+  config.id_links_to_resource = true
+  config.resource_controls_placement = :left
+  config.full_width_index_view = true
+  config.full_width_container = true
+  config.cache_resources_on_index_view = true
+  config.raise_error_on_missing_policy = true
+end
---
+++ spec/lib/string_extensions_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "StringExtensions" do
+  describe "#titleize_name" do
+    it "capitalizes the first letter of each word in a name" do
+      expect("john doe".titleize_name).to eq("John Doe")
+    end
+
+    it "handles uppercase names correctly" do
+      expect("JOHN DOE".titleize_name).to eq("John Doe")
+    end
+
+    it "handles mixed-case names correctly" do
+      expect("jOhN dOe".titleize_name).to eq("John Doe")
+    end
+
+    it "does not alter the casing of letters following an apostrophe" do
+      expect("juan d'arienzo".titleize_name).to eq("Juan D'Arienzo")
+    end
+
+    it "handles names with multiple parts correctly" do
+      expect("jean-claude van damme".titleize_name).to eq("Jean-Claude Van Damme")
+    end
+  end
+end
