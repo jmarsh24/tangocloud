fdfecd19334aefd68e0e8e3930e61e3445b46238
Justin Marsh <jmarsh24@gmail.com>
2024-01-24 20:23:33 +0100

    add some formatting to el recodo songs

--- app/avo/resources/el_recodo_song.rb
+++ app/avo/resources/el_recodo_song.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Avo::Resources::ElRecodoSong < Avo::BaseResource
+  self.title = "El Recodo Songs"
   self.includes = []
   self.index_query = -> {
     query.order(music_id: :asc)
@@ -13,24 +14,33 @@ class Avo::Resources::ElRecodoSong < Avo::BaseResource
       }
     end
   }
+  self.pagination = -> do
+    {
+      type: :countless,
+      size: []
+    }
+  end
 
   def fields
     field :id, as: :id, readonly: true, only_on: :show
     field :ert_number, as: :number, readonly: true, sortable: true, only_on: :show
     field :music_id, as: :number, readonly: true, sortable: true
-    field :title, as: :text, readonly: true
-    field :orchestra, as: :text, readonly: true
-    field :singer, as: :text, readonly: true
+    field :external, as: :text do
+      link_to "Link", "https://www.el-recodo.com/music?id=#{record.music_id}", target: "_blank", rel: "noopener"
+    end
+    field :title, as: :text, readonly: true, format_using: -> { value.truncate 28 }
+    field :orchestra, as: :text, readonly: true, format_using: -> { value.titleize_name }
     field :date, as: :date, readonly: true, sortable: true
-    field :style, as: :text, readonly: true
-    field :composer, as: :text, readonly: true
-    field :author, as: :text, readonly: true
-    field :soloist, as: :text, readonly: true
-    field :director, as: :text, readonly: true
-    field :label, as: :text, readonly: true
+    field :style, as: :text, readonly: true, format_using: -> { value.titleize.truncate 8 }
+    field :singer, as: :text, readonly: true, format_using: -> { value.truncate 24 }
+    field :author, as: :text, readonly: true, format_using: -> { value.truncate 24 }
+    field :composer, as: :text, readonly: true, format_using: -> { value.titleize_name }
+    field :soloist, as: :text, readonly: true, format_using: -> { value.titleize_name }
+    field :director, as: :text, readonly: true, format_using: -> { value.titleize_name }
+    field :label, as: :text, readonly: true, only_on: :show
     field :lyrics, as: :textarea, readonly: true, only_on: :show
-    field :synced_at, as: :date_time, readonly: true
-    field :page_updated_at, as: :date_time, readonly: true
+    field :synced_at, as: :date_time, readonly: true, only_on: :show
+    field :page_updated_at, as: :date_time, readonly: true, only_on: :show
   end
 
   def actions
