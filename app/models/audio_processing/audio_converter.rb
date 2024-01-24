da7b0344a6793dfc27364a538f478acac5b897c6
Justin Marsh <jmarsh24@gmail.com>
2024-01-25 00:04:58 +0100

    working

--- app/models/audio_processing/audio_converter.rb
+++ app/models/audio_processing/audio_converter.rb
@@ -5,11 +5,11 @@ module AudioProcessing
     attr_reader :file, :format, :bitrate, :sample_rate, :channels, :codec, :output_directory
 
     DEFAULT_OPTIONS = {
-      format: "m4a",
+      format: "mp3",
       bitrate: "320k",
       sample_rate: 48000,
       channels: 1,
-      codec: "alac",
+      codec: "libmp3lame",
       output_directory: "converted_audio_files",
       filename: nil
     }.freeze
@@ -28,20 +28,30 @@ module AudioProcessing
 
     def convert
       ensure_output_directory_exists
+      # cover_art_path = extract_cover_art # Extract cover art from the original file
       output = generate_output_filename
       movie = FFMPEG::Movie.new(file)
 
-      conversion_options = {
-        audio_codec: codec,
-        audio_bitrate: bitrate,
-        audio_sample_rate: sample_rate,
-        audio_channels: channels,
-        custom: ["-vn", "-movflags", "+faststart", "-map_metadata", "0", "-q:a", "2"]
-      }
+      custom_options = [
+        "-i", file,                          # Input audio file
+        # "-i", cover_art_path,                # Input cover art file, if it exists
+        "-map", "0:a:0",                     # Map the first (audio) stream from the first input (audio file)
+        "-codec:a", codec,                   # Audio codec
+        "-b:a", bitrate,                     # Audio bitrate (enforce 320k)
+        "-ar", sample_rate.to_s,             # Audio sample rate
+        "-ac", channels.to_s,                # Number of audio channels
+        "-movflags", "+faststart",           # Fast start for streaming
+        "-map_metadata", "0",                # Copy metadata from the first input (audio file)
+        "-id3v2_version", "3"                # Ensure compatibility with ID3v2
+      ]
+      # custom_options.insert(4, "-map", "1:v:0") if cover_art_path # Map the first (video/image) stream from the second input (cover art), if it exists
 
-      movie.transcode(output, conversion_options) do |progress|
+      movie.transcode(output, custom_options) do |progress|
         puts progress
       end
+
+      # Clean up the temporary cover art file
+      # FileUtils.rm(cover_art_path) if cover_art_path && File.exist?(cover_art_path)
 
       output
     rescue FFMPEG::Error => e
@@ -59,6 +69,16 @@ module AudioProcessing
       basename = @filename || File.basename(file, ".*")
       timestamp = Time.now.strftime("%Y%m%d%H%M%S")
       "#{@output_directory}/#{basename}_#{timestamp}.#{format}"
+    end
+
+    def extract_cover_art
+      cover_art_path = "#{output_directory}/cover_art.jpg"
+      command = "ffmpeg -i #{@file} -an -vcodec copy #{cover_art_path}"
+      system(command)
+      File.exist?(cover_art_path) ? cover_art_path : nil
+    rescue => e
+      puts "Failed to extract cover art: #{e.message}"
+      nil
     end
   end
 end
--- spec/models/import/el_recodo/song_scraper_spec.rb
+++ spec/models/import/el_recodo/song_scraper_spec.rb
@@ -4,62 +4,85 @@ require "rails_helper"
 
 RSpec.describe Import::ElRecodo::SongScraper do
   describe "#metadata" do
-    let(:music_id) { 1 }
-    let(:song_scraper) { described_class.new(music_id:) }
-    let(:html_fixture_path) { Rails.root.join("spec/fixtures/el_recodo_song.html") }
-    let(:html_content) { File.read(html_fixture_path) }
+    context "for normal songs" do
+      before do
+        music_1_html = Rails.root.join("spec/fixtures/el_recodo_music_id_1.html")
+        stub_request(:get, "https://www.el-recodo.com/music?id=1&lang=en")
+          .to_return(status: 200, body: File.read(music_1_html))
+      end
 
-    before do
-      stub_request(:get, "https://www.el-recodo.com/music?id=#{music_id}&lang=en")
-        .to_return(status: 200, body: html_content)
+      it "fetches and parses song metadata correctly" do
+        metadata = described_class.new(music_id: 1).metadata
+
+        expect(metadata.ert_number).to eq(1)
+        expect(metadata.title).to eq("Te burlas tristeza")
+        expect(metadata.date).to eq(Date.new(1960, 7, 28))
+        expect(metadata.style).to eq("TANGO")
+        expect(metadata.orchestra).to eq("Rodolfo BIAGI")
+        expect(metadata.singer).to eq("Hugo Duval")
+        expect(metadata.composer).to eq("Edmundo Baya")
+        expect(metadata.author).to eq("Julio César Curi")
+        expect(metadata.label).to eq("Columbia")
+        expect(metadata.music_id).to eq(1)
+        expect(metadata.page_updated_at).to eq(Time.zone.parse("2018-10-14 02:00:00.000000000 +0200"))
+      end
     end
 
-    it "fetches and parses song metadata correctly" do
-      metadata = song_scraper.metadata
+    context "for songs with director and soloist" do
+      before do
+        music_6417_html = Rails.root.join("spec/fixtures/el_recodo_music_id_6417.html")
+        stub_request(:get, "https://www.el-recodo.com/music?id=6417&lang=en")
+          .to_return(status: 200, body: File.read(music_6417_html))
+      end
 
-      expect(metadata.ert_number).to eq(1)
-      expect(metadata.title).to eq("Te burlas tristeza")
-      expect(metadata.date).to eq(Date.new(1960, 7, 28))
-      expect(metadata.style).to eq("TANGO")
-      expect(metadata.orchestra).to eq("Rodolfo BIAGI")
-      expect(metadata.singer).to eq("Hugo Duval")
-      expect(metadata.composer).to eq("Edmundo Baya")
-      expect(metadata.author).to eq("Julio César Curi")
-      expect(metadata.label).to eq("Columbia")
-      expect(metadata.music_id).to eq(1)
-      expect(metadata.page_updated_at).to eq(Time.zone.parse("2018-10-14 02:00:00.000000000 +0200"))
+      it "fetches and parses song metadata correctly" do
+        metadata = described_class.new(music_id: 6417).metadata
+        expect(metadata.ert_number).to eq(6417)
+        expect(metadata.title).to eq("Otra noche")
+        expect(metadata.date).to eq(Date.new(1944, 8, 8))
+        expect(metadata.style).to eq("TANGO")
+        expect(metadata.orchestra).to be_nil
+        expect(metadata.singer).to be_nil
+        expect(metadata.composer).to eq("Rodolfo Sciammarella")
+        expect(metadata.author).to eq("Rodolfo Sciammarella")
+        expect(metadata.soloist).to eq("Alberto CASTILLO")
+        expect(metadata.director).to eq("Dir. Emilio Balcarce")
+        expect(metadata.label).to be_nil
+        expect(metadata.music_id).to eq(6417)
+        expect(metadata.page_updated_at).to eq(Time.zone.parse("2013-8-15 02:00:00.000000000 +0200"))
+      end
     end
 
     context "when the server returns Too Many Requests (429)" do
       before do
-        stub_request(:get, "https://www.el-recodo.com/music?id=#{music_id}&lang=en")
+        stub_request(:get, "https://www.el-recodo.com/music?id=1&lang=en")
           .to_return(status: 429)
       end
 
       it "raises a TooManyRequestsError" do
-        expect { song_scraper.metadata }.to raise_error(Import::ElRecodo::SongScraper::TooManyRequestsError)
+        expect { described_class.new(music_id: 1).metadata }.to raise_error(Import::ElRecodo::SongScraper::TooManyRequestsError)
       end
     end
 
     context "when the page is not found (404)" do
       before do
-        stub_request(:get, "https://www.el-recodo.com/music?id=#{music_id}&lang=en")
+        stub_request(:get, "https://www.el-recodo.com/music?id=1&lang=en")
           .to_return(status: 404)
       end
 
       it "raises a PageNotFoundError" do
-        expect { song_scraper.metadata }.to raise_error(Import::ElRecodo::SongScraper::PageNotFoundError)
+        expect { described_class.new(music_id: 1).metadata }.to raise_error(Import::ElRecodo::SongScraper::PageNotFoundError)
       end
     end
 
     context "when the page is not found (302)" do
       before do
-        stub_request(:get, "https://www.el-recodo.com/music?id=#{music_id}&lang=en")
+        stub_request(:get, "https://www.el-recodo.com/music?id=1&lang=en")
           .to_return(status: 302)
       end
 
       it "raises a PageNotFoundError" do
-        expect { song_scraper.metadata }.to raise_error(Import::ElRecodo::SongScraper::PageNotFoundError)
+        expect { described_class.new(music_id: 1).metadata }.to raise_error(Import::ElRecodo::SongScraper::PageNotFoundError)
       end
     end
   end
