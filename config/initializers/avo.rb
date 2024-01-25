3874fb261455eee34c033c88ef7d7f52fd016476
Justin Marsh <jmarsh24@gmail.com>
2024-01-25 17:20:45 +0100

    working

--- config/initializers/avo.rb
+++ config/initializers/avo.rb
@@ -3,10 +3,10 @@
 Avo.configure do |config|
   config.current_user_method = :current_user
   config.per_page_steps = [48, 96, 256]
-  config.id_links_to_resource = true
   config.resource_controls_placement = :left
   config.full_width_index_view = true
   config.full_width_container = true
   config.cache_resources_on_index_view = true
   config.raise_error_on_missing_policy = true
+  config.home_path = -> { resources_el_recodo_songs_path }
 end
