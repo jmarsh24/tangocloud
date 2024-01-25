05a7e34fc27a6ff3a6ae3df4289ffa46480cff9a
Justin Marsh <jmarsh24@gmail.com>
2024-01-25 23:10:18 +0100

    add username generator

--- api/app/models/user_preference.rb
+++ api/app/models/user_preference.rb
@@ -22,6 +22,8 @@ class UserPreference < ApplicationRecord
   validates :user_id, presence: true
   validates :locale, inclusion: {in: ["en", "es"]}
 
+  before_validation :set_default_username, on: :create
+
   has_one_attached :avatar do |blob|
     blob.variant :small, resize_to_limit: [160, 160], saver: {strip: true, quality: 75, lossless: false, alpha_q: 85, reduction_effort: 6, smart_subsample: true}, format: "webp"
     blob.variant :large, resize_to_limit: [500, 500], saver: {strip: true, quality: 75, lossless: false, alpha_q: 85, reduction_effort: 6, smart_subsample: true}, format: "webp"
@@ -33,5 +35,22 @@ class UserPreference < ApplicationRecord
     else
       Gravatar.new(user.email).url(width:)
     end
+  end
+
+  private
+
+  def set_default_username
+    return if username.present?
+
+    base_username = user.email.split("@").first
+    new_username = base_username
+    counter = 1
+
+    while UserPreference.exists?(username: new_username)
+      new_username = "#{base_username}#{counter}"
+      counter += 1
+    end
+
+    self.username = new_username
   end
 end
