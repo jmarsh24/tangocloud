9976a524f37ac3fd88942651d48c83a524f385d3
Justin Marsh <jmarsh24@gmail.com>
2024-01-25 02:00:02 +0100

    metadata importer spec

---
+++ db/migrate/20240124235421_add_trigram_index_to_album_title.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddTrigramIndexToAlbumTitle < ActiveRecord::Migration[7.1]
+  def change
+    add_index :albums, :title, using: :gist, opclass: {title: :gist_trgm_ops}
+  end
+end
