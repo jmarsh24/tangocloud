d0e7374631f2d21c3ff9ec2d566623a0012ae227
Justin Marsh <jmarsh24@gmail.com>
2024-01-25 02:00:50 +0100

    fix indentation

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,29 +1,29 @@
-  # frozen_string_literal: true
+# frozen_string_literal: true
 
-  # == Schema Information
-  #
-  # Table name: audios
-  #
-  #  id          :integer          not null, primary key
-  #  duration    :integer          default(0), not null
-  #  format      :string           not null
-  #  bit_rate    :integer
-  #  sample_rate :integer
-  #  channels    :integer
-  #  bit_depth   :integer
-  #  created_at  :datetime         not null
-  #  updated_at  :datetime         not null
-  #
-  class Audio < ApplicationRecord
-    has_many :audio_transfers, dependent: :destroy
-    has_many :transfer_agents, through: :audio_transfers
+# == Schema Information
+#
+# Table name: audios
+#
+#  id          :integer          not null, primary key
+#  duration    :integer          default(0), not null
+#  format      :string           not null
+#  bit_rate    :integer
+#  sample_rate :integer
+#  channels    :integer
+#  bit_depth   :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Audio < ApplicationRecord
+  has_many :audio_transfers, dependent: :destroy
+  has_many :transfer_agents, through: :audio_transfers
 
-    validates :duration, presence: true
-    validates :format, presence: true
-    validates :bit_rate, numericality: {only_integer: true}
-    validates :sample_rate, numericality: {only_integer: true}
-    validates :channels, numericality: {only_integer: true}
-    validates :bit_depth, numericality: {only_integer: true}
+  validates :duration, presence: true
+  validates :format, presence: true
+  validates :bit_rate, numericality: {only_integer: true}
+  validates :sample_rate, numericality: {only_integer: true}
+  validates :channels, numericality: {only_integer: true}
+  validates :bit_depth, numericality: {only_integer: true}
 
-    has_one_attached :file, dependent: :purge_later
-  end
+  has_one_attached :file, dependent: :purge_later
+end
