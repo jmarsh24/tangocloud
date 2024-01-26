7b6fc430f2ace7a22a154522b1bf5f943da54715
Justin Marsh <jmarsh24@gmail.com>
2024-01-27 00:32:28 +0100

    rubocop

--- api/app/controllers/graphql_controller.rb
+++ api/app/controllers/graphql_controller.rb
@@ -14,9 +14,9 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = TangocloudSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = TangocloudSchema.execute(query, variables:, context:, operation_name:)
     render json: result
-  rescue StandardError => e
+  rescue => e
     raise e unless Rails.env.development?
     handle_error_in_development(e)
   end
@@ -47,6 +47,6 @@ class GraphqlController < ApplicationController
     logger.error e.message
     logger.error e.backtrace.join("\n")
 
-    render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: 500
+    render json: {errors: [{message: e.message, backtrace: e.backtrace}], data: {}}, status: :internal_server_error
   end
 end
