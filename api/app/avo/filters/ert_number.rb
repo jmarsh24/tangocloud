b82a1f5180fd1e77f2e4464265a04f49c4b0b068
Justin Marsh <jmarsh24@gmail.com>
2024-01-26 16:18:10 +0100

    filter by ertnumber

---
+++ api/app/avo/filters/ert_number.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Avo::Filters::ErtNumber < Avo::Filters::TextFilter
+  self.name = "Ert Number"
+  self.button_label = "Filter by ERT Number"
+  self.visible = -> do
+    true
+  end
+
+  def apply(request, query, value)
+    query.where(ert_number: value)
+  end
+end
