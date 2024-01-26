8f94f047822dfb54f1435e9db01ab3f65bc4bb8e
Justin Marsh <jmarsh24@gmail.com>
2024-01-27 00:30:29 +0100

    working date!!!

--- api/app/graphql/types/el_recodo_song_type.rb
+++ api/app/graphql/types/el_recodo_song_type.rb
@@ -6,7 +6,7 @@ module Types
     field :title, String, null: true
     field :artist, String, null: true
     field :album, String, null: true
-    field :date, Integer, null: true
+    field :date, GraphQL::Types::ISO8601Date, null: true
     field :genre, String, null: true
     field :lyrics, String, null: true
     field :ert_number, Integer, null: true
