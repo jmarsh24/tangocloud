802ff95f0872d938b18d3471223a526b0a2b387f
Justin Marsh <jmarsh24@gmail.com>
2024-01-27 13:12:38 +0100

    working

--- app/App.js
+++ app/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello</Text>
+      <Text>Dogac! We are going to change the Tango World!</Text>
       <StatusBar style="auto" />
     </View>
   );
