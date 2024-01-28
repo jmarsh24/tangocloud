430d5f7b8b555a86849fbdbc53a0c1dd84963267
Justin Marsh <jmarsh24@gmail.com>
2024-01-28 13:40:35 +0100

    try spec helper

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -5,17 +5,17 @@ RSpec.configure do |config|
   config.disable_monkey_patching!
   config.order = :random
 
-  # config.before(:suite) do
-  #   # reindex models
-  #   ElRecodoSong.reindex
+  config.before(:suite) do
+    # reindex models
+    ElRecodoSong.reindex
 
-  #   # and disable callbacks
-  #   Searchkick.disable_callbacks
-  # end
+    # and disable callbacks
+    Searchkick.disable_callbacks
+  end
 
-  # config.around(:each, search: true) do |example|
-  #   Searchkick.callbacks(nil) do
-  #     example.run
-  #   end
-  # end
+  config.around(:each, search: true) do |example|
+    Searchkick.callbacks(nil) do
+      example.run
+    end
+  end
 end
