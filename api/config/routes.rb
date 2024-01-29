fd9386c9b652c3066a3f9a48cc998b69bf191fd1
Justin Marsh <jmarsh24@gmail.com>
2024-01-29 01:10:06 +0100

    remove namespce constraint

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  constraints subdomain: "api" do
-    if Rails.env.development?
-      mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-    end
-    mount ActionAuth::Engine => "action_auth"
-    constraints AdminConstraint do
-      mount GoodJob::Engine => "good_job"
-      mount Avo::Engine => "admin"
-    end
-
-    post "/graphql", to: "graphql#execute"
-
-    root "pages#home"
-    get "up", to: "rails/health#show", as: :rails_health_check
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+  mount ActionAuth::Engine => "action_auth"
+  constraints AdminConstraint do
+    mount GoodJob::Engine => "good_job"
+    mount Avo::Engine => "admin"
+  end
+
+  post "/graphql", to: "graphql#execute"
+
+  root "pages#home"
+  get "up", to: "rails/health#show", as: :rails_health_check
 end
