5e58a8dd7c855ba913d495e0cc5b14c5f752ed99
Justin Marsh <jmarsh24@gmail.com>
2024-01-30 11:47:00 +0100

    update sync song spec

--- api/app/jobs/import/el_recodo/sync_song_job.rb
+++ api/app/jobs/import/el_recodo/sync_song_job.rb
@@ -5,8 +5,9 @@ class Import::ElRecodo::SyncSongJob < ApplicationJob
   queue_as :background_sync
   good_job_control_concurrency_with(perform_limit: 1)
 
-  def perform(music_id:, interval:)
+  def perform(music_id:, interval: 0)
     sleep(interval)
     ::Import::ElRecodo::SongSynchronizer.new.sync_song(music_id:)
+  rescue Import::ElRecodo::SongScraper::PageNotFoundError
   end
 end
--- api/spec/jobs/import/el_recodo/sync_song_job_spec.rb
+++ api/spec/jobs/import/el_recodo/sync_song_job_spec.rb
@@ -6,15 +6,22 @@ RSpec.describe Import::ElRecodo::SyncSongJob, type: :job do
   describe "#perform" do
     it "calls sync_song on SongSynchronizer with correct parameters" do
       music_id = 123
-      interval = 5
       song_synchronizer_instance = instance_double("Import::ElRecodo::SongSynchronizer")
       allow(Import::ElRecodo::SongSynchronizer).to receive(:new).and_return(song_synchronizer_instance)
       allow(song_synchronizer_instance).to receive(:sync_song)
 
-      Import::ElRecodo::SyncSongJob.perform_now(music_id:, interval:)
+      Import::ElRecodo::SyncSongJob.perform_now(music_id:)
 
       expect(Import::ElRecodo::SongSynchronizer).to have_received(:new)
       expect(song_synchronizer_instance).to have_received(:sync_song).with(music_id:)
+    end
+
+    it "does not raise an error if SongScraper::PageNotFoundError is raised" do
+      music_id = 123
+      song_synchronizer_instance = instance_double("Import::ElRecodo::SongSynchronizer")
+      allow(Import::ElRecodo::SongSynchronizer).to receive(:new).and_return(song_synchronizer_instance)
+      allow(song_synchronizer_instance).to receive(:sync_song).and_raise(Import::ElRecodo::SongScraper::PageNotFoundError)
+      expect { Import::ElRecodo::SyncSongJob.perform_now(music_id:) }.not_to raise_error
     end
   end
 end
