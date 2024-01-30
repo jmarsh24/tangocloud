d794915a4c7a2efc1821ba40375200e3ce9374e6
Justin Marsh <jmarsh24@gmail.com>
2024-01-30 23:31:05 +0100

    add a header

--- app/app/(tabs)/library.tsx
+++ app/app/(tabs)/library.tsx
@@ -1,39 +1,70 @@
-import { ActivityIndicator, FlatList } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
 import TrackListItem from '@/components/TrackListItem';
 import { gql, useQuery } from '@apollo/client';
+import Colors from '@/constants/Colors';
 
 const query = gql`
   query MyQuery($q: String!) {
-    searchElRecodoSongs(query: "*") {
-              id
-              title
-              orchestra
-              singer
-              composer
-              author
-              date
-              style
-            }
+    searchElRecodoSongs(query: $q) {
+      id
+      title
+      orchestra
+      singer
+      composer
+      author
+      date
+      style
+    }
   }
 `;
 
 export default function LibraryScreen() {
-  const { data, loading, error } = useQuery(query);
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: "*" },
+  });
 
   if (loading) {
-    return <ActivityIndicator />;
+    return <ActivityIndicator size="large" color={Colors.light.tint} />;
   }
+
   if (error) {
-    console.log(error);
+    console.error(error);
+    return <Text style={styles.errorText}>Error loading tracks</Text>;
   }
-  console.log(data);
+
   const tracks = data?.searchElRecodoSongs || [];
 
   return (
-    <FlatList
-      data={tracks}
-      renderItem={({ item }) => <TrackListItem track={item} />}
-      showsVerticalScrollIndicator={false}
-    />
+    <View style={styles.container}>
+      <Text style={styles.header}>Library</Text>
+      <FlatList
+        data={tracks}
+        renderItem={({ item }) => <TrackListItem track={item} />}
+        keyExtractor={item => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: Colors.light.background,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    paddingHorizontal: 15,
+    marginBottom: 10,
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+});
