2bbe1563b92f815c0dc7e9a7513ce790e3e391a0
Justin Marsh <jmarsh24@gmail.com>
2024-01-31 10:53:53 +0100

    add pwned

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
   validates :password, allow_nil: true, length: {minimum: 12}
+  validates :password, not_pwned: {message: "might easily be guessed"}
 
   normalizes :email, with: -> { _1.strip.downcase }
 
