1831949677b62c966fb1e0d6db8467041280f5fd
Justin Marsh <jmarsh24@gmail.com>
2024-01-31 02:54:48 +0100

    set up sentry

--- api/config/initializers/sentry.rb
+++ api/config/initializers/sentry.rb
@@ -3,9 +3,13 @@
 require "sentry-ruby"
 
 Sentry.init do |config|
+  sentry_environment = Config.sentry_environment || Rails.env
+  next unless Rails.env.production? || Rails.env.staging?
   config.dsn = Config.sentry_dsn
+
   config.breadcrumbs_logger = [:sentry_logger, :http_logger]
 
+  config.environment = sentry_environment
   config.enabled_environments = [sentry_environment].filter_map(&:presence).excluding("development", "test")
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
