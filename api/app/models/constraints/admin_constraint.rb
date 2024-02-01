ae6445ff27b2bc48a8de3ebcf8f900764d9dc671
Justin Marsh <jmarsh24@gmail.com>
2024-02-01 17:43:04 +0100

    update constraints

--- api/app/models/constraints/admin_constraint.rb
+++ api/app/models/constraints/admin_constraint.rb
@@ -1,8 +1,6 @@
-# app/constraints/admin_constraint.rb
-
-class Constraints::AdminConstraint
-  def self.matches?(request)
-    if (session = Session.find_by(request.cookie_jar.signed[session_token]))
+class AdminConstraint
+  def matches?(request)
+    if session = Session.find_by_id(request.cookie_jar.signed[:session_token])
       session.user.admin?
     else
       false
