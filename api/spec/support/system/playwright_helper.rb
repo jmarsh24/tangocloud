7ba41c2b7aa66b9a9b1bc09a74500b048ee4c780
Justin Marsh <jmarsh24@gmail.com>
2024-02-01 17:59:26 +0000

    dev container working

---
+++ api/spec/support/system/playwright_helper.rb
@@ -0,0 +1,24 @@
+module CupriteHelper
+  def pause
+    Capybara.current_session.driver.with_playwright_page do |page|
+      page.context.enable_debug_console!
+      page.pause
+    end
+  end
+
+  def screenshot(name)
+    return unless Config.screenshots?
+
+    width = page.current_window.size[0]
+    height = page.current_window.size[1]
+    super("#{name}--desktop")
+    page.current_window.resize_to(375, height)
+    super("#{name}--mobile")
+    page.current_window.resize_to(width, height)
+  end
+end
+
+RSpec.configure do |config|
+  config.include Capybara::Screenshot::Diff::TestMethods, type: :system
+  config.include CupriteHelper, type: :system
+end
