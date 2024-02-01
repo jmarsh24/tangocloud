e7af605d709d9151b1c76f23235fe417ea9ebd67
Justin Marsh <jmarsh24@gmail.com>
2024-02-02 00:28:02 +0100

    fix converting from saving

--- api/app/models/audio_processing/audio_converter.rb
+++ api/app/models/audio_processing/audio_converter.rb
@@ -26,50 +26,31 @@ module AudioProcessing
     end
 
     def convert
-      ensure_output_directory_exists
-      output = generate_output_filename
+      Dir.mkdir(output_directory) unless Dir.exist?(output_directory)
+      Tempfile.create([@filename || File.basename(file, ".*"), ".#{format}"], @output_directory) do |tempfile|
+        output = tempfile.path
 
-      custom_options = [
-        "-i", file,                          # Input audio file
-        "-map", "0:a:0",                     # Map the first (audio) stream from the first input (audio file)
-        "-codec:a", codec,                   # Audio codec
-        "-b:a", bitrate,                     # Audio bitrate (enforce 320k)
-        "-ar", sample_rate.to_s,             # Audio sample rate
-        "-ac", channels.to_s,                # Number of audio channels
-        "-movflags", "+faststart",           # Fast start for streaming
-        "-id3v2_version", "3"                # Ensure compatibility with ID3v2
-      ]
+        custom_options = [
+          "-i", file,                          # Input audio file
+          "-map", "0:a:0",                     # Map the first (audio) stream from the first input (audio file)
+          "-codec:a", codec,                   # Audio codec
+          "-b:a", bitrate,                     # Audio bitrate (enforce 320k)
+          "-ar", sample_rate.to_s,             # Audio sample rate
+          "-ac", channels.to_s,                # Number of audio channels
+          "-movflags", "+faststart",           # Fast start for streaming
+          "-id3v2_version", "3"                # Ensure compatibility with ID3v2
+        ]
 
-      @movie.transcode(output, custom_options) do |progress|
-        puts progress
+        @movie.transcode(output, custom_options) do |progress|
+          puts progress
+        end
+
+        yield output if block_given?
+
+        output
       end
-
-      yield output if block_given?
-      output
     rescue FFMPEG::Error => e
       puts "Failed to convert file: #{e.message}"
-      nil
-    end
-
-    private
-
-    def ensure_output_directory_exists
-      Dir.mkdir(output_directory) unless Dir.exist?(output_directory)
-    end
-
-    def generate_output_filename
-      basename = @filename || File.basename(file, ".*")
-      timestamp = Time.now.strftime("%Y%m%d%H%M%S")
-      "#{@output_directory}/#{basename}_#{timestamp}.#{format}"
-    end
-
-    def extract_cover_art
-      cover_art_path = "#{output_directory}/cover_art.jpg"
-      command = "ffmpeg -i #{@file} -an -vcodec copy #{cover_art_path}"
-      system(command)
-      File.exist?(cover_art_path) ? cover_art_path : nil
-    rescue => e
-      puts "Failed to extract cover art: #{e.message}"
       nil
     end
   end
--- api/spec/models/audio_processing/audio_converter_spec.rb
+++ api/spec/models/audio_processing/audio_converter_spec.rb
@@ -5,23 +5,22 @@ RSpec.describe AudioProcessing::AudioConverter do
   let(:output_directory) { "tmp/converted_audio_files" }
   let(:converter) { AudioProcessing::AudioConverter.new(file: test_file_path, output_directory:) }
 
-  before do
-    FileUtils.mkdir_p(output_directory) unless Dir.exist?(output_directory)
-  end
-
-  after do
-    FileUtils.rm_rf(output_directory) if Dir.exist?(output_directory)
-  end
-
   describe "#convert" do
-    it "converts the file to the specified format" do
-      output = converter.convert
-      expect(File.extname(output)).to eq(".aac")
-      basename = File.basename(test_file_path, ".*")
-      expect(output).to include(basename)
-      expect(output).to match(/\d{14}/)
-      expect(File.dirname(output)).to eq(output_directory)
-      expect(output.start_with?(output_directory)).to be_truthy
+    it "converts the file to the specified format and deletes it afterward" do
+      converted_file_path = nil
+
+      converter.convert do |output|
+        expect(File.extname(output)).to eq(".aac")
+        basename = File.basename(test_file_path, ".*")
+        expect(output).to include(basename)
+        expect(File.dirname(output)).to eq(output_directory)
+        expect(output.start_with?(output_directory)).to be_truthy
+        expect(File.exist?(output)).to be_truthy
+
+        converted_file_path = output
+      end
+
+      expect(File.exist?(converted_file_path)).to be_falsey
     end
   end
 end
