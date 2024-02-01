db3040e98a91d090e48352c47190c524bd1a65a7
Justin Marsh <jmarsh24@gmail.com>
2024-02-02 00:14:21 +0100

    cover art

--- api/spec/models/audio_processing/cover_art_extractor_spec.rb
+++ api/spec/models/audio_processing/cover_art_extractor_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe AudioProcessing::CoverArtExtractor do
   describe "#extract_cover_art" do
     it "extracts the cover art from the file" do
-      file = Rails.root.join("spec", "fixtures", "029_-_Nunca_tuvo_novio.aif")
+      file = Rails.root.join("spec", "fixtures", "audio", "19380307_comme_il_faut_instrumental_tango_2758.aif")
       described_class.new(file:).extract_cover_art do |tempfile|
         expect(tempfile.path).to match(/cover_art/)
         expect(tempfile.path).to match(/.jpg/)
