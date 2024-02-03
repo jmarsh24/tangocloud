18bd87474c22929846a209a9e485e91e730d45d3
Justin Marsh <jmarsh24@gmail.com>
2024-02-03 13:34:51 +0100

    rename to tanda recording

--- api/app/policies/tanda_recording_policy.rb
+++ api/app/policies/tanda_recording_policy.rb
@@ -1,4 +1,4 @@
-class TandaAudioTransferPolicy < ApplicationPolicy
+class TandaRecordingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
