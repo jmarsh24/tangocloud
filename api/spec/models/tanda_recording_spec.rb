0a97a7ed4db5e6573aacfaf39602f38a07e71fce
Justin Marsh <jmarsh24@gmail.com>
2024-02-03 15:16:03 +0100

    update server

--- api/spec/models/tanda_recording_spec.rb
+++ api/spec/models/tanda_recording_spec.rb
@@ -6,12 +6,12 @@ end
 
 # == Schema Information
 #
-# Table name: tanda_audio_transfers
+# Table name: tanda_recordings
 #
-#  id                :uuid             not null, primary key
-#  position          :integer          default(0), not null
-#  tanda_id          :uuid             not null
-#  audio_transfer_id :uuid             not null
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
+#  id           :uuid             not null, primary key
+#  position     :integer          default(0), not null
+#  tanda_id     :uuid             not null
+#  recording_id :uuid             not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
