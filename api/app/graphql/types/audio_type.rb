c1463f1544ae66bbca792d0ccdfb2e81c2ed5a08
Justin Marsh <jmarsh24@gmail.com>
2024-02-03 19:49:54 +0100

    update audio type

--- api/app/graphql/types/audio_type.rb
+++ api/app/graphql/types/audio_type.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 module Types
   class AudioType < Types::BaseObject
-    field :id, ID, null: true
-    field :duration, Integer, null: true
-    field :format, String, null: true
-    field :codec, String, null: true
-    field :bit_rate, Integer, null: true
-    field :sample_rate, Integer, null: true
-    field :channels, Integer, null: true
-    field :length, Integer, null: true
-    field :metadata, GraphQL::Types::JSON, null: true
-    field :audio_transfer_id, ID, null: true
-    field :created_at, GraphQL::Types::ISO8601Date, null: true
-    field :updated_at, GraphQL::Types::ISO8601Date, null: true
+    field :id, ID, null: false
+    field :duration, Integer, null: false
+    field :format, String, null: false
+    field :codec, String, null: false
+    field :bit_rate, Integer
+    field :sample_rate, Integer
+    field :channels, Integer
+    field :length, Integer, null: false
+    field :metadata, GraphQL::Types::JSON, null: false
+    field :audio_transfer_id, Types::UuidType, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
