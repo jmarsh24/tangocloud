1cf6271367e3f436435c7f4dc0ee2335f3a3a9ee
Justin Marsh <jmarsh24@gmail.com>
2024-02-04 14:08:22 +0100

    remove resolver

--- api/app/graphql/resolvers/base_resolver.rb
+++ api/app/graphql/resolvers/base_resolver.rb
@@ -1,16 +1,4 @@
 module Resolvers
   class BaseResolver < GraphQL::Schema::Resolver
-    def resolve_audio(id:)
-      audio = Audio.find(id)
-      url = generate_signed_url(audio)
-      {
-        id: audio.id,
-        url:
-      }
-    end
-
-    def generate_signed_url(audio)
-      Rails.application.routes.url_helpers.rails_blob_path(audio.file, disposition: "attachment")
-    end
   end
 end
