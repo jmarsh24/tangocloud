37f2a72041cf2e8fa9255f13960178cfe5aa1364
Justin Marsh <jmarsh24@gmail.com>
2024-02-07 21:43:57 +0100

    add sync method

--- api/app/models/import/music/directory_importer.rb
+++ api/app/models/import/music/directory_importer.rb
@@ -21,6 +21,29 @@ module Import
           end
         end
       end
+
+      def sync
+        return unless Dir.exist?(@directory)
+
+        # Fetch all filenames from the database
+        existing_filenames = Audio.all.with_attached_file.map { |audio| audio.file.filename.to_s }
+
+        Dir.glob(File.join(@directory, "**", "*")).each do |file|
+          next if File.directory?(file) # Skip directories
+
+          mime_type = MIME::Types.type_for(file).first
+          next unless mime_type && SongImporter::SUPPORTED_MIME_TYPES.include?(mime_type.content_type)
+
+          # Skip if the filename already exists in the database
+          next if existing_filenames.include?(File.basename(file))
+
+          begin
+            SongImporter.new(file:).sync
+          rescue => e
+            Rails.logger.error "Failed to sync #{file}: #{e.message}"
+          end
+        end
+      end
     end
   end
 end
