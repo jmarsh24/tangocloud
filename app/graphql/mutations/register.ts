294d0212f87ccbbe678c57595dfb5004655d0e9b
Justin Marsh <jmarsh24@gmail.com>
2024-02-10 21:32:15 +0100

    add letter opener

--- app/graphql/mutations/register.ts
+++ app/graphql/mutations/register.ts
@@ -1,17 +1,14 @@
 import { gql } from "@apollo/client";
 
 export const REGISTER_MUTATION = gql`
-  mutation Register($username: String!, $email: String!, $password: String!) {
-    register(
-      input: { username: $username, email: $email, password: $password }
-    ) {
+  mutation signUp($username: String!, $email: String!, $password: String!) {
+    signUp(input: { username: $username, email: $email, password: $password }) {
       user {
         id
         username
         email
         name
       }
-      token
     }
   }
 `;
