3b8970bfb1734bd32fdf9a5801b0fae7485b3195
Justin Marsh <jmarsh24@gmail.com>
2024-02-10 14:51:02 +0100

    updaet backend

--- api/app/controllers/api/graphql_controller.rb
+++ api/app/controllers/api/graphql_controller.rb
@@ -1,6 +1,7 @@
 module Api
   class GraphQLController < ApplicationController
     # protect_from_forgery with: :null_session
+    skip_after_action :verify_authorized
 
     def execute
       variables = prepare_variables(params[:variables])
