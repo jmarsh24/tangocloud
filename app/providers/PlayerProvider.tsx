53af5ebe1609b7bde9af1319c359e24fe538d02a
Justin Marsh <jmarsh24@gmail.com>
2024-02-13 20:48:54 +0100

    Switch to react-native-track-player (#94)
    * initial commit for Switch to react-native-track-player, fixes #93
    
    * initial commit
    
    * perhaps better?
    
    * MUSIC WORKING
    
    * pieces in place
    
    * refactor
    
    * add track player controls
    
    * add play pause buton to player
    
    * render trackInfo into player
    
    * add packages
    
    * run prebuild
    
    * add flashlist
    
    * working player
    
    * upgrade app json
    
    * update schema

--- app/providers/PlayerProvider.tsx
+++ app/providers/PlayerProvider.tsx
@@ -1,81 +1,25 @@
-import React, { createContext, useState, useContext, useEffect, PropsWithChildren } from 'react';
-import { Audio } from 'expo-av';
-import * as SecureStore from 'expo-secure-store';
-import { Track } from '@/types'; // Assuming you have a Track type defined
+import React, { createContext, useContext, PropsWithChildren } from 'react';
 
 const PlayerContext = createContext({
-  track: undefined,
-  setTrack: (track: Track) => {},
-  playTrack: () => {},
-  pauseTrack: () => {},
-  isPlaying: false,
+  // track: undefined,
+  // setTrack: (track: Track) => {},
+  // playTrack: () => {},
+  // pauseTrack: () => {},
+  // isPlaying: false,
 });
 
-async function getAuthToken(): Promise<string | null> {
-  return await SecureStore.getItemAsync('token');
-}
+// async function getAuthToken(): Promise<string | null> {
+//   return await SecureStore.getItemAsync('token');
+// }
 
 export default function PlayerProvider({ children }: PropsWithChildren) {
-  const [track, setTrack] = useState<Track>();
-  const [sound, setSound] = useState<Audio.Sound>();
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  useEffect(() => {
-    return sound
-      ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync();
-        }
-      : undefined;
-  }, [sound]);
-
-  useEffect(() => {
-    const setAudioMode = async () => {
-      await Audio.setAudioModeAsync({
-        playsInSilentModeIOS: true,
-        shouldDuckAndroid: true,
-        staysActiveInBackground: true,
-      });
-    };
-
-    setAudioMode();
-  }, []);
-
-  const playTrack = async () => {
-    if (!track) return;
-
-    const audioUrl = track.audios[0].url;
-    if (sound) {
-      await sound.unloadAsync();
-    }
-    if (!audioUrl) return;
-
-    const authToken = await getAuthToken();
-    const source = {
-      uri: audioUrl,
-      headers: {
-        Authorization: authToken ? `Bearer ${authToken}` : '',
-      },
-    };
-
-    const { sound: newSound } = await Audio.Sound.createAsync(source);
-    setSound(newSound);
-    newSound.setOnPlaybackStatusUpdate((status) => setIsPlaying(status.isPlaying));
-    await newSound.playAsync();
-  };
-
-  const pauseTrack = async () => {
-    if (sound && isPlaying) {
-      await sound.pauseAsync();
-    }
-  };
 
   const providerValue = {
-    track,
-    setTrack,
-    playTrack,
-    pauseTrack,
-    isPlaying,
+    // track,
+    // setTrack,
+    // playTrack,
+    // pauseTrack,
+    // isPlaying,
   };
 
   return <PlayerContext.Provider value={providerValue}>{children}</PlayerContext.Provider>;
