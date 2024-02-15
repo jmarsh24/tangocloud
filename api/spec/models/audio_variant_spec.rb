5c761c5afddf3bb736884fe7446565a1e723a6d3
Justin Marsh <jmarsh24@gmail.com>
2024-02-16 00:56:11 +0100

    fixtures fixed

--- api/spec/models/audio_variant_spec.rb
+++ api/spec/models/audio_variant_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe AudioVariant, type: :model do
   describe "#signed_url" do
     it "returns the URL of the audio" do
       freeze_time
-      audio_variant = audio_variants(:volver_a_sonar_tango_tunes_1940)
+      audio_variant = audio_variants(:volver_a_sonar_tango_tunes_1940_audio_variant)
       expected_url = "http://localhost:3000/api/audio_variants/#{audio_variant.signed_id}"
 
       expect(audio_variant.signed_url).to eq(expected_url)
