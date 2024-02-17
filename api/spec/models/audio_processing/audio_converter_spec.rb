42479a313be7fef6f9251798480c64f05261ad90
Justin Marsh <jmarsh24@gmail.com>
2024-02-17 12:11:14 +0100

    audio converter working

--- api/spec/models/audio_processing/audio_converter_spec.rb
+++ api/spec/models/audio_processing/audio_converter_spec.rb
@@ -3,28 +3,22 @@ require "rails_helper"
 RSpec.describe AudioProcessing::AudioConverter do
   describe "#convert" do
     it "converts the file to the specified format and deletes it afterward" do
-      converted_file_path = nil
-      output_directory = "tmp/converted_audio"
-      file = "spec/fixtures/audio/tone.mp3"
+      file = File.open("spec/fixtures/audio/tone.mp3")
+      output_path = nil
 
-      AudioProcessing::AudioConverter.new(file:, output_directory: "tmp/converted_audio").convert do |output|
-        expect(File.extname(output)).to eq(".aac")
-        basename = File.basename(file, ".*")
-        expect(output).to include(basename)
-        expect(File.dirname(output)).to eq(output_directory)
-        expect(output.start_with?(output_directory)).to be_truthy
-        expect(File.exist?(output)).to be_truthy
-
-        converted_file_path = output
+      AudioProcessing::AudioConverter.new(file).convert do |output|
+        converted_movie = FFMPEG::Movie.new(output.path)
+        expect(converted_movie.audio_codec).to eq("aac")
+        output_path = output.path
       end
 
-      expect(File.exist?(converted_file_path)).to be_falsey unless Config.ci?
+      expect(File.exist?(output_path)).to be_falsey unless Config.ci?
     end
 
     it "removes all metadata from a flac file" do
-      file = "spec/fixtures/audio/19401008_volver_a_sonar_roberto_rufino_tango_2476.flac"
+      file = File.open("spec/fixtures/audio/19401008_volver_a_sonar_roberto_rufino_tango_2476.flac")
 
-      AudioProcessing::AudioConverter.new(file:).convert do |output|
+      AudioProcessing::AudioConverter.new(file).convert do |output|
         extracted_metadata = AudioProcessing::MetadataExtractor.new(file: output).extract_metadata
 
         non_nil_keys = [:duration, :bit_rate, :sample_rate, :channels, :format, :bit_depth, :codec_name, :codec_long_name]
