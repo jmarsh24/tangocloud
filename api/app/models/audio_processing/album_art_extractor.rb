5e44ecb92802dfbe5208be0a616c1c359c885771
Justin Marsh <jmarsh24@gmail.com>
2024-02-17 12:51:35 +0100

    album art extractor refactored

--- api/app/models/audio_processing/album_art_extractor.rb
+++ api/app/models/audio_processing/album_art_extractor.rb
@@ -1,42 +1,31 @@
 module AudioProcessing
   class AlbumArtExtractor
-    attr_reader :file, :output
+    class MissingAlbumArtError < StandardError; end
+    attr_reader :file
 
-    def initialize(file:, output: "/tmp")
-      @file = file.to_s
-      @output = output
+    def initialize(file)
+      @file = file
     end
 
     def extract
-      movie = FFMPEG::Movie.new(file)
-      album_art_path = nil
+      Tempfile.create(["album_art", ".jpg"]) do |tempfile|
+        movie = FFMPEG::Movie.new(file.path)
 
-      output_filename = File.basename(file, ".*") + ".jpg"
-      full_output_path = File.join(output, output_filename)
+        movie.transcode(tempfile.path,
+          {
+            vcodec: "mjpeg",
+            qscale: "1",
+            frame_rate: "1",
+            frames: "1",
+            custom: ["-an"]
+          })
 
-      Tempfile.create(["album_art", ".jpg"], binmode: true) do |tempfile|
-        options = {vcodec: "mjpeg", qscale: "1", frame_rate: "1", frames: "1", custom: ["-an"]}
-
-        begin
-          movie.transcode(tempfile.path, options)
-
-          if File.exist?(tempfile.path) && !tempfile.size.zero?
-            yield(tempfile) if block_given?
-
-            FileUtils.mkdir_p(File.dirname(full_output_path))
-
-            FileUtils.cp(tempfile.path, full_output_path)
-
-            album_art_path = full_output_path
-          else
-            puts "No album art found in the file."
-          end
-        rescue FFMPEG::Error => e
-          puts "Error extracting album art: #{e.message}"
+        if File.exist?(tempfile.path) && !tempfile.size.zero?
+          yield tempfile if block_given?
         end
       end
-
-      album_art_path
+    rescue FFMPEG::Error
+      raise MissingAlbumArtError
     end
   end
 end
