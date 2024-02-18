c3415b2bf0cf4126403d4ebec9633a4a3d0ff5fb
Justin Marsh <jmarsh24@gmail.com>
2024-02-18 11:18:45 +0100

    add dark mode to tracks

--- app/components/TrackInfo.tsx
+++ app/components/TrackInfo.tsx
@@ -1,38 +1,35 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { useTheme } from '@react-navigation/native';
 import type { Track } from 'react-native-track-player';
 
 export const TrackInfo: React.FC<{
   track?: Track;
 }> = ({ track }) => {
+  const { colors } = useTheme(); // Use the current theme colors
+
+  // Update styles to use theme colors
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+    },
+    titleText: {
+      fontSize: 18,
+      fontWeight: '600',
+      color: colors.text, // Use theme color
+      marginTop: 30,
+    },
+    artistText: {
+      fontSize: 16,
+      fontWeight: '200',
+      color: colors.text, // Use theme color
+    },
+  });
+
   return (
     <View style={styles.container}>
-      {/* <Image style={styles.artwork} source={{ uri: track?.artwork }} /> */}
       <Text style={styles.titleText}>{track?.title}</Text>
       <Text style={styles.artistText}>{track?.artist}</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-  },
-  artwork: {
-    width: '60%',
-    aspectRatio: 1,
-    marginTop: '2%',
-    backgroundColor: 'grey',
-  },
-  titleText: {
-    fontSize: 18,
-    fontWeight: '600',
-    color: 'white',
-    marginTop: 30,
-  },
-  artistText: {
-    fontSize: 16,
-    fontWeight: '200',
-    color: 'white',
-  },
-});
