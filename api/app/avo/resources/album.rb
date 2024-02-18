22a1216dcae49982cd3413ba2d4bbf29cc5d76d1
Justin Marsh <jmarsh24@gmail.com>
2024-02-18 13:19:19 +0100

    update album

--- api/app/avo/resources/album.rb
+++ api/app/avo/resources/album.rb
@@ -5,17 +5,15 @@ class Avo::Resources::Album < Avo::BaseResource
   # }
 
   def fields
-    field :id, as: :id, only_on: [:show]
+    field :id, as: :id
     field :title, as: :text
-    field :description, as: :textarea, only_on: [:show, :edit, :new]
-    field :release_date, as: :date, only_on: [:show, :edit, :new]
-    field :album_type, as: :select, enum: ::Album.types
-    field :recordings_count, as: :number
+    field :description, as: :textarea
+    field :release_date, as: :date
+    field :audio_transfers_count, as: :number
     field :slug, as: :text
     field :external_id, as: :text
-    field :transfer_agent_id, as: :text
+    field :album_type, as: :select, enum: ::Album.album_types
     field :album_art, as: :file
-    field :transfer_agent, as: :belongs_to
-    field :recordings, as: :has_many
+    field :audio_transfers, as: :has_many
   end
 end
