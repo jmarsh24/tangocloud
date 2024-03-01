5cf34520758c14bcce6d5884f3f5e532dce965a3
Justin Marsh <jmarsh24@gmail.com>
2024-03-01 20:52:45 +0100

    rubocop

--- api/app/graphql/tangocloud_schema.rb
+++ api/app/graphql/tangocloud_schema.rb
@@ -6,7 +6,6 @@ class TangocloudSchema < GraphQL::Schema
   use GraphQL::Dataloader
   use GraphQL::Tracing::NewRelicTracing, set_transaction_name: true
 
-
   # GraphQL-Ruby calls this when something goes wrong while running a query:
   def self.type_error(err, context)
     # if err.is_a?(GraphQL::InvalidNullError)
