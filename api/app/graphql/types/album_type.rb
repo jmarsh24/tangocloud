1e7321c42c0b3161c71c5a45c843eb512a3dbc54
Justin Marsh <jmarsh24@gmail.com>
2024-03-01 09:50:47 +0100

    merge with main

--- api/app/graphql/types/album_type.rb
+++ api/app/graphql/types/album_type.rb
@@ -8,14 +8,8 @@ module Types
     field :slug, String, null: true
     field :external_id, String, null: true
     field :album_type, String, null: true
-    field :album_art_url, String, null: true
+    field :album_art, Types::ImageType, null: true
 
     has_many :audio_transfers
-
-    def album_art_url
-      if object.album_art.attached?
-        Rails.application.routes.url_helpers.rails_blob_url(object.album_art)
-      end
-    end
   end
 end
