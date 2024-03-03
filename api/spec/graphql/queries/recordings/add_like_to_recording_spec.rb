0de6b6c790985fecd4b72c3b0f8a32936dc99141
Justin Marsh <jmarsh24@gmail.com>
2024-03-03 17:30:19 +0100

    add like to recording  working

--- api/spec/graphql/queries/recordings/add_like_to_recording_spec.rb
+++ api/spec/graphql/queries/recordings/add_like_to_recording_spec.rb
@@ -1,57 +1,44 @@
 require "rails_helper"
 
-RSpec.describe "liked", type: :request do
-  describe "query" do
-    it "returns true if the user has liked the likeable" do
-      user = users(:normal)
-      recording = recordings(:volver_a_sonar)
-      recording.likes.create(user:)
-      token = AuthToken.token(user)
-
-      query = <<~GQL
-        query {
-          liked(likeableType: "Recording", likeableId: "#{recording.id}")
+RSpec.describe "liked", type: :graph do
+  let(:admin_user) { users(:admin) }
+  let(:normal_user) { users(:normal) }
+  let(:recording) { recordings(:volver_a_sonar) }
+  let(:mutation) do
+    <<~GQL
+      mutation AddLikeToRecording($recordingId: ID!) {
+        addLikeToRecording(input: { recordingId: $recordingId}) {
+          like {
+            id
+            likeableType
+            likeableId
+            user {
+              id
+            }
+          }
+          success
+          errors
         }
-      GQL
+      }
+    GQL
+  end
 
-      post api_graphql_path, params: {query:}, headers: {"Authorization" => "Bearer #{token}"}
+  describe "AddLikeToRecording" do
+    it "creates a like" do
+      gql(mutation, variables: {recordingId: recording.id}, user: admin_user)
 
-      json = JSON.parse(response.body)
-      data = json.dig("data", "liked")
-      expect(data).to be(true)
+      expect(data.add_like_to_recording.success).to be_truthy
+      expect(data.add_like_to_recording.errors).to be_blank
+      expect(data.add_like_to_recording.like.likeable_type).to eq("Recording")
+      expect(data.add_like_to_recording.like.likeable_id).to eq(recording.id.to_s)
+      expect(data.add_like_to_recording.like.user.id).to eq(admin_user.id.to_s)
     end
 
-    it "returns false if the user has not liked the likeable" do
-      user = users(:normal)
-      recording = recordings(:volver_a_sonar)
-      token = AuthToken.token(user)
+    it "returns error if user alread liked recording" do
+      gql(mutation, variables: {recordingId: recording.id}, user: normal_user)
 
-      query = <<~GQL
-        query {
-          liked(likeableType: "Recording", likeableId: "#{recording.id}")
-        }
-      GQL
-
-      post api_graphql_path, params: {query:}, headers: {"Authorization" => "Bearer #{token}"}
-
-      json = JSON.parse(response.body)
-      data = json["data"]["liked"]
-      expect(data).to be(false)
-    end
-
-    it "returns an error if the user is not authenticated" do
-      recording = recordings(:volver_a_sonar)
-
-      query = <<~GQL
-        query {
-          liked(likeableType: "Recording", likeableId: "#{recording.id}")
-        }
-      GQL
-
-      post api_graphql_path, params: {query:}
-
-      json = JSON.parse(response.body)
-      expect(json.dig("errors")[0]).to eq("You must be signed in to access this resource.")
+      expect(data.add_like_to_recording.success).to be_falsey
+      expect(data.add_like_to_recording.errors).to eq(["User has already liked this"])
     end
   end
 end
