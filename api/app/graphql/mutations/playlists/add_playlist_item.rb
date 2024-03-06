5ae6a0b8f309b7b175e6d440035b0ae900ab2246
Justin Marsh <jmarsh24@gmail.com>
2024-03-06 21:37:09 +0100

    addPlaylistItem

--- api/app/graphql/mutations/playlists/add_playlist_item.rb
+++ api/app/graphql/mutations/playlists/add_playlist_item.rb
@@ -1,21 +1,27 @@
 module Mutations::Playlists
   class AddPlaylistItem < Mutations::BaseMutation
     argument :playlist_id, ID, required: true
-    argument :recording_id, ID, required: true
+    argument :item_id, ID, required: true
+    argument :position, Integer, required: false
 
     field :playlist, Types::PlaylistType, null: true
     field :errors, [String], null: true
 
-    def resolve(playlist_id:, recording_id:)
+    def resolve(playlist_id:, item_id:, position: nil)
       playlist = Playlist.find(playlist_id)
-      recording = Recording.find(recording_id)
-
-      playlist_item = PlaylistItem.new(playlist:, recording:)
-
-      if playlist_item.save
-        {playlist:}
+      audio_transfer = AudioTransfer.find(item_id)
+      playlist_audio_transfer = playlist.playlist_audio_transfers.build(audio_transfer:)
+      playlist_audio_transfer.insert_at(position) if position.present?
+      if playlist_audio_transfer.save
+        {
+          playlist:,
+          errors: []
+        }
       else
-        {errors: playlist_item.errors.full_messages}
+        {
+          playlist: nil,
+          errors: playlist_song.errors.full_messages
+        }
       end
     end
   end
