7ec1d110bf5b6d512c0ec045acef13ce379e8f19
Justin Marsh <jmarsh24@gmail.com>
2024-03-06 22:56:57 +0100

    update playlist

--- api/app/graphql/mutations/playlists/update_playlist.rb
+++ api/app/graphql/mutations/playlists/update_playlist.rb
@@ -1,19 +1,33 @@
 module Mutations::Playlists
   class UpdatePlaylist < Mutations::BaseMutation
     argument :id, ID, required: true
-    argument :title, String, required: true
-    argument :description, String, required: true
+    argument :title, String, required: false
+    argument :description, String, required: false
+    argument :image, ApolloUploadServer::Upload, required: false
 
     field :playlist, Types::PlaylistType, null: true
     field :errors, [String], null: true
 
-    def resolve(id:, title:, description:)
+    def resolve(id:, title:, description:, image: nil)
       playlist = Playlist.find(id)
 
-      if playlist.update(title:, description:)
-        {playlist:}
+      playlist.title = title
+      playlist.description = description
+
+      if image.present?
+        playlist.image.attach(io: File.open(image), filename: image.original_filename, content_type: image.content_type)
+      end
+
+      if playlist.save
+        {
+          playlist:,
+          errors: []
+        }
       else
-        {errors: playlist.errors.full_messages}
+        {
+          playlist: nil,
+          errors: playlist.errors.full_messages
+        }
       end
     end
   end
--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -12,5 +12,6 @@ module Types
     field :remove_like_from_recording, mutation: Mutations::Recordings::RemoveLikeFromRecording
     field :remove_listen, mutation: Mutations::ListenHistories::RemoveListen
     field :reorder_playlist_items, mutation: Mutations::Playlists::ReorderPlaylistItems
+    field :update_playlist, mutation: Mutations::Playlists::UpdatePlaylist
   end
 end
