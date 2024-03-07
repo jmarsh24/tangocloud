2b3c887c957cb76864413d92495c0d45633ee88d
Justin Marsh <jmarsh24@gmail.com>
2024-03-07 18:54:11 +0100

    working spec

--- api/spec/graphql/queries/playlists/fetch_playlist_spec.rb
+++ api/spec/graphql/queries/playlists/fetch_playlist_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Fetch Playlist", type: :graph do
   describe "Fetching playlist details" do
     let!(:user) { users(:normal) }
     let!(:playlist) { playlists(:awesome_playlist) }
-    let!(:audio_transfer) { audio_transfers(:volver_a_sonar_rufino_19401008_flac) }
+    let!(:recording) { recordings(:volver_a_sonar) }
     let!(:audio_variant) { audio_variants(:volver_a_sonar_rufino_aac) }
 
     let(:query) do
@@ -13,13 +13,19 @@ RSpec.describe "Fetch Playlist", type: :graph do
           fetchPlaylist(id: $id) {
             id
             title
-            playlistAudioTransfers {
+            playlistItems {
               id
-              audioTransfer {
-                id
-                audioVariants {
+              playable {
+                ... on Recording {
                   id
-                  audioFileUrl
+                  title
+                  audioTransfers {
+                    id
+                    audioVariants {
+                      id
+                      audioFileUrl
+                    }
+                  }
                 }
               }
             }
@@ -28,17 +34,25 @@ RSpec.describe "Fetch Playlist", type: :graph do
       GQL
     end
 
-    it "returns the correct playlist details, including audio transfers and variants" do
+    it "returns the correct playlist details, including recordings and audio variants" do
       gql(query, variables: {id: playlist.id.to_s}, user:)
 
       playlist_data = data.fetch_playlist
 
       expect(playlist_data.title).to eq(playlist.title)
       expect(playlist_data.id).to eq(playlist.id.to_s)
-      first_playlist_audio_transfer_data = playlist_data.playlist_audio_transfers.first
-      expect(first_playlist_audio_transfer_data).not_to be_nil
 
-      first_audio_transfer_data = first_playlist_audio_transfer_data.audio_transfer
+      first_playlist_item_data = playlist_data.playlist_items.first
+      expect(first_playlist_item_data).not_to be_nil
+
+      playable_data = first_playlist_item_data.playable
+      expect(playable_data).not_to be_nil
+
+      recording_data = playable_data
+      expect(recording_data.id).to eq(recording.id.to_s)
+      expect(recording_data.title).to eq(recording.title)
+
+      first_audio_transfer_data = recording_data.audio_transfers.first
       expect(first_audio_transfer_data).not_to be_nil
 
       first_audio_variant_data = first_audio_transfer_data.audio_variants.first
