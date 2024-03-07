9fcef2e2125021dee2b93746b99fd8e29742befb
Justin Marsh <jmarsh24@gmail.com>
2024-03-07 19:42:38 +0100

    playlist item working

--- api/app/graphql/types/playlist_item_type.rb
+++ api/app/graphql/types/playlist_item_type.rb
@@ -3,20 +3,12 @@
 module Types
   class PlaylistItemType < Types::BaseObject
     field :id, ID, null: false
-    field :playlist_id, ID, null: false
     field :playable_type, String, null: false
-    field :playable_id, ID, null: false
     field :position, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    field :playable, Types::PlayableType, null: false
-
-    def playable
-      dataloader.with(Sources::Preload, :playable).load(object)
-      object.playable
-    end
-
     belongs_to :playlist
+    belongs_to :playable, type: Types::PlayableType, null: false, association: :playable
   end
 end
