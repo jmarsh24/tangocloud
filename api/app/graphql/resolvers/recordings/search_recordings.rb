2118d3f5d95ef902686c7754c33b5bb4329b181c
Justin Marsh <jmarsh24@gmail.com>
2024-03-08 17:56:30 +0100

    fix search

--- api/app/graphql/resolvers/recordings/search_recordings.rb
+++ api/app/graphql/resolvers/recordings/search_recordings.rb
@@ -4,7 +4,7 @@ module Resolvers::Recordings
 
     argument :query, String, required: false, description: "Query to search for."
 
-    def resolve(query)
+    def resolve(query:)
       raise GraphQL::ExecutionError, "Authentication is required to access this query." unless context[:current_user]
 
       Recording.search_recordings(query || "*").results
--- api/app/models/recording.rb
+++ api/app/models/recording.rb
@@ -25,7 +25,7 @@ class Recording < ApplicationRecord
 
   enum recording_type: {studio: "studio", live: "live"}
 
-  def self.search_recordings(query = "*")
+  def self.search_recordings(query)
     Recording.search(query,
       fields: [
         "title",
