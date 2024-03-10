0584152105c5fd8c2a182e54cf2298b9da970661
Justin Marsh <jmarsh24@gmail.com>
2024-03-10 14:12:17 +0100

    DONE

--- app/app/(tabs)/browse/index.tsx
+++ app/app/(tabs)/browse/index.tsx
@@ -1,16 +1,69 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Stack, Link } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+import { useTheme } from '@react-navigation/native';
 
 const BrowseScreen = () => {
+  const { colors } = useTheme();
   return (
-    <View>
-      <Link push href="/orchestras">Orchestras</Link>
-      <Link push href="/singers">Singers</Link>
-      <Link push href="/composers">Composers</Link>
-      <Link push href="/lyricists">Lyricists</Link>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Link style={styles.link} push href="/orchestras">
+          <View style={[styles.button, { borderColor: colors.primary }]}>
+            <Text style={[styles.buttonText, { color: colors.text }]}>Orchestras</Text>
+          </View>
+        </Link>
+        <Link style={styles.link} push href="/singers">
+          <View style={[styles.button, { borderColor: colors.primary }]}>
+            <Text style={[styles.buttonText, { color: colors.text }]}>Singers</Text>
+          </View>
+        </Link>
+      </View>
+      <View style={styles.row}>
+        <Link style={styles.link} push href="/composers">
+          <View style={[styles.button, { borderColor: colors.primary }]}>
+            <Text style={[styles.buttonText, { color: colors.text }]}>Composers</Text>
+          </View>
+        </Link>
+        <Link style={styles.link} push href="/lyricists">
+          <View style={[styles.button, { borderColor: colors.primary }]}>
+            <Text style={[styles.buttonText, { color: colors.text }]}>Lyricists</Text>
+          </View>
+        </Link>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 20,  
+    justifyContent: 'flex-start',
+    padding: 20,
+  },
+  row: {
+    gap: 20, 
+    flexDirection: 'row',
+    justifyContent: 'space-around', 
+  },
+  link: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    borderWidth: 2,
+    borderRadius: 20,
+  },
+  button: {
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+  },
+});
 
 export default BrowseScreen;
--- app/app/(tabs)/singers/_layout.tsx
+++ app/app/(tabs)/singers/_layout.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { Stack } from "expo-router";
 
 const SingersLayout = () => {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Singers' }} />
+      <Stack.Screen name="[id]" options={{ title: 'Singer' }} />
+    </Stack>
+  )
 };
 
 export default SingersLayout;
