fddd472a161fc7c6924cc3813c9cf5b9aee902ab
Justin Marsh <jmarsh24@gmail.com>
2024-03-11 10:44:01 +0100

    remove playback history

--- api/app/avo/resources/playback_history.rb
+++
@@ -1,9 +0,0 @@
-class Avo::Resources::PlaybackHistory < Avo::BaseResource
-  self.includes = []
-
-  def fields
-    field :id, as: :id
-    field :user, as: :belongs_to
-    field :playbacks, as: :has_many
-  end
-end
