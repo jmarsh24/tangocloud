2350bf0f9246c0735e55977f5c2e655871cc3ff7
Justin Marsh <jmarsh24@gmail.com>
2024-04-13 19:01:56 +0200

    skip authenticate action

--- api/app/controllers/audio_variants_controller.rb
+++ api/app/controllers/audio_variants_controller.rb
@@ -2,9 +2,10 @@ class AudioVariantsController < ApplicationController
   include Rails.application.routes.url_helpers
 
   before_action :set_audio_variant, only: :show
-  skip_after_action :verify_authorized, only: :show # this should be removed if you want to use the policy
+  skip_before_action :authenticate
 
   def show
+    authorize @audio_variant
     redirect_to url_for(@audio_variant.audio_file)
   end
 
