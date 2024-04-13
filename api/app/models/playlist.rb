8cf53ab6b5f55a2c90b38fd4d2558f758b87079c
Justin Marsh <jmarsh24@gmail.com>
2024-04-13 14:30:44 +0200

    attach after_create_commit

--- api/app/models/playlist.rb
+++ api/app/models/playlist.rb
@@ -5,7 +5,7 @@ class Playlist < ApplicationRecord
   searchkick word_middle: [:title]
 
   before_validation :set_default_title
-  before_commit :attach_default_image, if: -> { image.blank? }
+  after_create_commit :attach_default_image, if: -> { image.blank? }
 
   validates :title, presence: true
 
