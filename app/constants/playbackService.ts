185a7c713451539143bda119300f6c7a5c19b3a0
Justin Marsh <jmarsh24@gmail.com>
2024-04-18 16:07:19 +0200

    swap-track-player (#124)
    * initial commit for swap-track-player
    
    * fix

---
+++ app/constants/playbackService.ts
@@ -0,0 +1,27 @@
+import TrackPlayer, { Event } from "react-native-track-player";
+
+export const playbackService = async () => {
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.play();
+  });
+
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    TrackPlayer.pause();
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteStop, () => {
+    TrackPlayer.stop();
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteNext, () => {
+    TrackPlayer.skipToNext();
+  });
+
+  TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+    TrackPlayer.skipToPrevious();
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteSeek, (event) => {
+    TrackPlayer.seekTo(event.position);
+  });
+};
