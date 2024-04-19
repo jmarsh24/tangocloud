22558f9fb7aecf99134977b1374ac7170aa663d8
Justin Marsh <jmarsh24@gmail.com>
2024-04-19 12:43:24 +0200

    working

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,83 +1,80 @@
-import { useEffect, useCallback } from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useColorScheme, StatusBar } from 'react-native';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack, SplashScreen } from 'expo-router';
-import { useFonts } from 'expo-font';
-import TrackPlayer from 'react-native-track-player';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
+import { useCallback, useEffect } from 'react'
+import { StatusBar, useColorScheme } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import TrackPlayer from 'react-native-track-player'
 
-import { playbackService } from '@/constants/playbackService';
-import { useLogTrackPlayerState } from '@/hooks/useLogTrackPlayerState';
-import { useSetupTrackPlayer } from '@/hooks/useSetupTrackPlayer';
-import ApolloClientProvider from '@/providers/ApolloClientProvider';
-import { AuthProvider } from '@/providers/AuthProvider';
+import { playbackService } from '@/constants/playbackService'
+import { useLogTrackPlayerState } from '@/hooks/useLogTrackPlayerState'
+import { useSetupTrackPlayer } from '@/hooks/useSetupTrackPlayer'
+import ApolloClientProvider from '@/providers/ApolloClientProvider'
+import { AuthProvider } from '@/providers/AuthProvider'
 
-export {
-  ErrorBoundary,
-} from 'expo-router';
+export { ErrorBoundary } from 'expo-router'
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 TrackPlayer.registerPlaybackService(() => playbackService)
 
 const App = () => {
+	const [fontsLoaded, fontsError] = useFonts({
+		SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
+		...FontAwesome.font,
+	})
 
-  const [fontsLoaded, fontsError] = useFonts({
-    SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+	const colorScheme = useColorScheme()
+	const statusBarStyle = colorScheme === 'dark' ? 'light-content' : 'dark-content'
 
-  const colorScheme = useColorScheme();
-  const statusBarStyle = colorScheme === 'dark' ? 'light-content' : 'dark-content';
+	const handleTrackPlayerLoaded = useCallback(() => {
+		SplashScreen.hideAsync()
+	}, [])
 
-  const handleTrackPlayerLoaded = useCallback(() => {
-    SplashScreen.hideAsync()
-  }, []);
+	useSetupTrackPlayer({
+		onLoad: handleTrackPlayerLoaded,
+	})
 
-  useSetupTrackPlayer({
-    onLoad: handleTrackPlayerLoaded,
-  });
+	useLogTrackPlayerState()
 
-  useLogTrackPlayerState();
+	useEffect(() => {
+		if (fontsError) throw fontsError
+		if (fontsLoaded) SplashScreen.hideAsync()
+	}, [fontsLoaded, fontsError])
 
-  useEffect(() => {
-    if (fontsError) throw fontsError;
-    if (fontsLoaded) SplashScreen.hideAsync();
-  }, [fontsLoaded, fontsError]);
+	if (!fontsLoaded) {
+		return null
+	}
 
-  if (!fontsLoaded) {
-    return null;
-  }
+	return (
+		<SafeAreaProvider>
+			<GestureHandlerRootView style={{ flex: 1 }}>
+				<RootLayoutNav />
 
-  return (
-    <SafeAreaProvider>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <RootLayoutNav />
-
-        <StatusBar barStyle={statusBarStyle} />
-      </GestureHandlerRootView>
-    </SafeAreaProvider>
-  );
+				<StatusBar barStyle={statusBarStyle} />
+			</GestureHandlerRootView>
+		</SafeAreaProvider>
+	)
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme()
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <ApolloClientProvider>
-        <AuthProvider>
-          <Stack>
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="player/[id]" options={{ presentation: 'modal', headerShown: false }} />
-          </Stack>
-        </AuthProvider>
-      </ApolloClientProvider>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+			<ApolloClientProvider>
+				<AuthProvider>
+					<Stack>
+						<Stack.Screen name="(auth)" options={{ headerShown: false }} />
+						<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+						<Stack.Screen name="player" options={{ presentation: 'modal', headerShown: false }} />
+					</Stack>
+				</AuthProvider>
+			</ApolloClientProvider>
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App
---
+++ app/store/library.tsx
@@ -0,0 +1,97 @@
+import library from '@/assets/data/library.json'
+import { unknownTrackImageUri } from '@/constants/images'
+import { Artist, Playlist, TrackWithPlaylist } from '@/helpers/types'
+import { Track } from 'react-native-track-player'
+import { create } from 'zustand'
+
+interface LibraryState {
+	tracks: TrackWithPlaylist[]
+	toggleTrackFavorite: (track: Track) => void
+	addToPlaylist: (track: Track, playlistName: string) => void
+}
+
+export const useLibraryStore = create<LibraryState>()((set) => ({
+	tracks: library,
+	toggleTrackFavorite: (track) =>
+		set((state) => ({
+			tracks: state.tracks.map((currentTrack) => {
+				if (currentTrack.url === track.url) {
+					return {
+						...currentTrack,
+						rating: currentTrack.rating === 1 ? 0 : 1,
+					}
+				}
+
+				return currentTrack
+			}),
+		})),
+	addToPlaylist: (track, playlistName) =>
+		set((state) => ({
+			tracks: state.tracks.map((currentTrack) => {
+				if (currentTrack.url === track.url) {
+					return {
+						...currentTrack,
+						playlist: [...(currentTrack.playlist ?? []), playlistName],
+					}
+				}
+
+				return currentTrack
+			}),
+		})),
+}))
+
+export const useTracks = () => useLibraryStore((state) => state.tracks)
+
+export const useFavorites = () => {
+	const favorites = useLibraryStore((state) => state.tracks.filter((track) => track.rating === 1))
+	const toggleTrackFavorite = useLibraryStore((state) => state.toggleTrackFavorite)
+
+	return {
+		favorites,
+		toggleTrackFavorite,
+	}
+}
+
+export const useArtists = () =>
+	useLibraryStore((state) => {
+		return state.tracks.reduce((acc, track) => {
+			const existingArtist = acc.find((artist) => artist.name === track.artist)
+
+			if (existingArtist) {
+				existingArtist.tracks.push(track)
+			} else {
+				acc.push({
+					name: track.artist ?? 'Unknown',
+					tracks: [track],
+				})
+			}
+
+			return acc
+		}, [] as Artist[])
+	})
+
+export const usePlaylists = () => {
+	const playlists = useLibraryStore((state) => {
+		return state.tracks.reduce((acc, track) => {
+			track.playlist?.forEach((playlistName) => {
+				const existingPlaylist = acc.find((playlist) => playlist.name === playlistName)
+
+				if (existingPlaylist) {
+					existingPlaylist.tracks.push(track)
+				} else {
+					acc.push({
+						name: playlistName,
+						tracks: [track],
+						artworkPreview: track.artwork ?? unknownTrackImageUri,
+					})
+				}
+			})
+
+			return acc
+		}, [] as Playlist[])
+	})
+
+	const addToPlaylist = useLibraryStore((state) => state.addToPlaylist)
+
+	return { playlists, addToPlaylist }
+}
