74e21b607f9a1bbf7d126bf3c5ebf2f8c60aa2ce
Justin Marsh <jmarsh24@gmail.com>
2024-04-20 17:55:36 +0200

    update events

--- app/constants/playbackService.ts
+++ app/constants/playbackService.ts
@@ -1,27 +1,35 @@
-import TrackPlayer, { Event } from "react-native-track-player";
+import TrackPlayer, { Event } from 'react-native-track-player'
 
 export const playbackService = async () => {
-  TrackPlayer.addEventListener(Event.RemotePlay, () => {
-    TrackPlayer.play();
-  });
+	TrackPlayer.addEventListener(Event.RemotePause, () => {
+		TrackPlayer.pause()
+	})
 
-  TrackPlayer.addEventListener(Event.RemotePause, () => {
-    TrackPlayer.pause();
-  });
+	TrackPlayer.addEventListener(Event.RemotePlay, () => {
+		TrackPlayer.play()
+	})
 
-  TrackPlayer.addEventListener(Event.RemoteStop, () => {
-    TrackPlayer.stop();
-  });
+	TrackPlayer.addEventListener(Event.RemoteNext, () => {
+		TrackPlayer.skipToNext()
+	})
 
-  TrackPlayer.addEventListener(Event.RemoteNext, () => {
-    TrackPlayer.skipToNext();
-  });
+	TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+		TrackPlayer.skipToPrevious()
+	})
 
-  TrackPlayer.addEventListener(Event.RemotePrevious, () => {
-    TrackPlayer.skipToPrevious();
-  });
+	TrackPlayer.addEventListener(Event.RemoteJumpForward, async (event) => {
+		TrackPlayer.seekBy(event.interval)
+	})
 
-  TrackPlayer.addEventListener(Event.RemoteSeek, (event) => {
-    TrackPlayer.seekTo(event.position);
-  });
-};
+	TrackPlayer.addEventListener(Event.RemoteJumpBackward, async (event) => {
+		TrackPlayer.seekBy(-event.interval)
+	})
+
+	TrackPlayer.addEventListener(Event.RemoteSeek, async (event) => {
+		TrackPlayer.seekTo(event.position)
+	})
+
+	TrackPlayer.addEventListener(Event.RemoteStop, () => {
+		TrackPlayer.stop()
+	})
+}
