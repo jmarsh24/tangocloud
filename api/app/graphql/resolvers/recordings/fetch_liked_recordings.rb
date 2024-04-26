df3cf663efa4a850985200aa32250652816bbd0f
Justin Marsh <jmarsh24@gmail.com>
2024-04-26 13:10:50 +0200

    limit history to 200

--- api/app/graphql/resolvers/recordings/fetch_liked_recordings.rb
+++ api/app/graphql/resolvers/recordings/fetch_liked_recordings.rb
@@ -5,7 +5,7 @@ module Resolvers::Recordings
     def resolve
       raise GraphQL::ExecutionError, "Authentication is required to access this query." unless context[:current_user]
 
-      Recording.joins(:likes).where(likes: {user_id: context[:current_user].id, likeable_type: "Recording"})
+      Recording.joins(:likes).where(likes: {user_id: context[:current_user].id, likeable_type: "Recording"}).order(likes: {created_at: :desc}).limit(200).distinct
     end
   end
 end
