b7769dd9032ec72c5567d5cb8084014d2b96327f
Justin Marsh <jmarsh24@gmail.com>
2024-04-26 13:18:48 +0200

    remove uniqueness

--- api/app/graphql/resolvers/recordings/fetch_liked_recordings.rb
+++ api/app/graphql/resolvers/recordings/fetch_liked_recordings.rb
@@ -5,7 +5,7 @@ module Resolvers::Recordings
     def resolve
       raise GraphQL::ExecutionError, "Authentication is required to access this query." unless context[:current_user]
 
-      Recording.joins(:likes).where(likes: {user_id: context[:current_user].id, likeable_type: "Recording"}).order(likes: {created_at: :desc}).limit(200).distinct
+      Recording.joins(:likes).where(likes: {user_id: context[:current_user].id, likeable_type: "Recording"}).order(likes: {created_at: :desc})
     end
   end
 end
