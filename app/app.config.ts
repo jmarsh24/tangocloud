376d8156daf9b1bff1d90b819f737da3c2808099
Justin Marsh <jmarsh24@gmail.com>
2024-04-27 12:01:49 +0200

    add version number

---
+++ app/app.config.ts
@@ -0,0 +1,69 @@
+import { ExpoConfig } from 'expo/config'
+
+export default (): ExpoConfig => {
+	return {
+		name: 'Tango Cloud',
+		slug: 'app',
+		version: '1.0.24',
+		owner: 'tangocloud',
+		orientation: 'portrait',
+		icon: './assets/icon.png',
+		userInterfaceStyle: 'dark',
+		scheme: 'tangocloudapp',
+		splash: {
+			image: './assets/splash.png',
+			resizeMode: 'contain',
+			backgroundColor: '#ffffff',
+		},
+		assetBundlePatterns: ['**/*'],
+		ios: {
+			supportsTablet: true,
+			bundleIdentifier: 'tangocloud',
+			runtimeVersion: '1.0.0',
+			buildNumber: '67',
+			config: {
+				usesNonExemptEncryption: false,
+			},
+			infoPlist: {
+				UIBackgroundModes: ['audio'],
+				NSMicrophoneUsageDescription: 'This app needs access to the speaker for streaming music.',
+				ITSAppUsesNonExemptEncryption: false,
+			},
+			associatedDomains: ['applinks:tangocloud.app'],
+		},
+		android: {
+			package: 'com.tangocloud.app',
+			versionCode: 35,
+			runtimeVersion: {
+				policy: 'appVersion',
+			},
+			adaptiveIcon: {
+				foregroundImage: './assets/adaptive-icon.png',
+				backgroundColor: '#FFFFFF',
+			},
+		},
+		plugins: [
+			'expo-router',
+			[
+				'expo-secure-store',
+				{
+					faceIDPermission: 'Allow $(PRODUCT_NAME) to access your Face ID biometric data.',
+				},
+			],
+			'expo-font',
+		],
+		experiments: {
+			typedRoutes: true,
+			tsconfigPaths: true,
+		},
+		extra: {
+			eas: {
+				projectId: '40b28cff-7ae4-44c3-b2b4-da1eb1d5081b',
+			},
+			currentVersion: '1.0.24',
+		},
+		updates: {
+			url: 'https://u.expo.dev/40b28cff-7ae4-44c3-b2b4-da1eb1d5081b',
+		},
+	}
+}
