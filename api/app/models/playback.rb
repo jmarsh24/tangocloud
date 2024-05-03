4f184c399fb3de1b56e5b24d1d897d6209a07e7d
Justin Marsh <jmarsh24@gmail.com>
2024-05-03 17:08:33 +0200

    try flipping direction

--- api/app/models/playback.rb
+++ api/app/models/playback.rb
@@ -2,7 +2,7 @@ class Playback < ApplicationRecord
   belongs_to :user
   belongs_to :recording, counter_cache: true
 
-  scope :most_recent, -> { order(created_at: :desc) }
+  scope :most_recent, -> { order(created_at: :asc) }
 end
 
 # == Schema Information
