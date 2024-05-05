b6b9faac6439311ef4a79bc718d853ee1b0d4ea0
Justin Marsh <jmarsh24@gmail.com>
2024-05-05 11:34:54 +0200

    update you

--- app/graphql/queries/user_profile.ts
+++ app/graphql/queries/user_profile.ts
@@ -1,49 +1,54 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client'
 
 export const USER_PROFILE = gql`
-  query UserProfile {
-    userProfile {
-      id
-      email
-      username
-      name
-      firstName
-      lastName
-      verified
-      admin
-      avatarUrl
-      createdAt
-      updatedAt
-      playbacks {
-        edges {
-          node {
-            id
-            recording {
-              id
-              title
-              orchestra {
-                name
-              }
-              genre {
-                name
-              }
-              year
-              singers {
-                name
-              }
-              audioTransfers {
-                audioVariants {
-                  audioFileUrl
-                  duration
-                }
-                album {
-                  albumArtUrl
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+	query UserProfile {
+		userProfile {
+			id
+			email
+			username
+			name
+			firstName
+			lastName
+			verified
+			admin
+			avatarUrl
+			createdAt
+			updatedAt
+			playbacks {
+				edges {
+					node {
+						id
+						recording {
+							id
+							title
+							orchestra {
+								name
+							}
+							genre {
+								name
+							}
+							year
+							singers {
+								name
+							}
+							composition {
+								lyrics {
+									content
+								}
+							}
+							audioTransfers {
+								audioVariants {
+									audioFileUrl
+									duration
+								}
+								album {
+									albumArtUrl
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`
