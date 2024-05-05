7ae567c4e80672eb28a956de56776da86c18be2c
Justin Marsh <jmarsh24@gmail.com>
2024-05-05 11:34:30 +0200

    update search

--- app/graphql/queries/search_recordings.ts
+++ app/graphql/queries/search_recordings.ts
@@ -1,41 +1,46 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client'
 
 export const SEARCH_RECORDINGS = gql`
-  query SearchRecordings($query: String, $first: Int, $after: String) {
-    searchRecordings(query: $query, first: $first, after: $after) {
-      edges {
-        node {
-          id
-          title
-          recordedDate
-          orchestra {
-            name
-          }
-          singers {
-            name
-          }
-          genre {
-            name
-          }
-          year
-          audioTransfers {
-            album {
-              albumArtUrl
-            }
-            audioVariants {
-              id
-              duration
-              audioFileUrl
-            }
-          }
-        }
-      }
-      pageInfo {
-        endCursor
-        startCursor
-        hasNextPage
-        hasPreviousPage
-      }
-    }
-  }
-`;
+	query SearchRecordings($query: String, $first: Int, $after: String) {
+		searchRecordings(query: $query, first: $first, after: $after) {
+			edges {
+				node {
+					id
+					title
+					recordedDate
+					orchestra {
+						name
+					}
+					singers {
+						name
+					}
+					genre {
+						name
+					}
+					composition {
+						lyrics {
+							content
+						}
+					}
+					year
+					audioTransfers {
+						album {
+							albumArtUrl
+						}
+						audioVariants {
+							id
+							duration
+							audioFileUrl
+						}
+					}
+				}
+			}
+			pageInfo {
+				endCursor
+				startCursor
+				hasNextPage
+				hasPreviousPage
+			}
+		}
+	}
+`
