b18c12805875d1d62d61f26395716931f08793bd
Justin Marsh <jmarsh24@gmail.com>
2024-05-06 10:00:00 +0200

    add to query

--- app/graphql/queries/fetch_liked_recordings.ts
+++ app/graphql/queries/fetch_liked_recordings.ts
@@ -1,36 +1,41 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client'
 
 export const FETCH_LIKED_RECORDINGS = gql`
-  query FetchLikedRecordings {
-    fetchLikedRecordings {
-      edges {
-        node {
-          id
-          title
-          year
-          genre {
-            name
-          }
-          singers {
-            name
-          }
-          orchestra {
-            name
-          }
-          audioTransfers {
-            id
-            album {
-              id
-              albumArtUrl
-            }
-            audioVariants {
-              id
-              audioFileUrl
-              duration
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+	query FetchLikedRecordings {
+		fetchLikedRecordings {
+			edges {
+				node {
+					id
+					title
+					year
+					genre {
+						name
+					}
+					singers {
+						name
+					}
+					orchestra {
+						name
+					}
+					composition {
+						lyrics {
+							content
+						}
+					}
+					audioTransfers {
+						id
+						album {
+							id
+							albumArtUrl
+						}
+						audioVariants {
+							id
+							audioFileUrl
+							duration
+						}
+					}
+				}
+			}
+		}
+	}
+`
