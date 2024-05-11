d940a0564eb3fd89c9bbeecc01551430af4f5872
Justin Marsh <jmarsh24@gmail.com>
2024-05-11 15:29:14 +0200

    remove redirect

--- api/app/controllers/recordings_controller.rb
+++ api/app/controllers/recordings_controller.rb
@@ -3,8 +3,7 @@ class RecordingsController < ApplicationController
   skip_before_action :authenticate
 
   def show
-    app_url = "tangocloudapp://recordings/#{params[:id]}"
-
-    redirect_to app_url, allow_other_host: true
+    @recording = Recording.find!(params[:id])
+    @deeplink_url = "tangocloudapp://recordings/#{params[:id]}"
   end
 end
