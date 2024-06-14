4ff1b3acbe47062dec61dc845c41d3b4260eb383
Justin Marsh <jmarsh24@gmail.com>
2024-06-14 10:40:54 +0200

    adjust login screen

--- app/app/(auth)/_layout.tsx
+++ app/app/(auth)/_layout.tsx
@@ -1,32 +1,36 @@
-import { useAuth } from '@/providers/AuthProvider';
-import { Redirect, Stack } from 'expo-router';
-import { View } from 'react-native';
-import { defaultStyles } from '@/styles';
-import { StackScreenWithSearchBar } from '@/constants/layout';
+import { StackScreenWithSearchBar } from '@/constants/layout'
+import { useAuth } from '@/providers/AuthProvider'
+import { defaultStyles } from '@/styles'
+import { Redirect, Stack } from 'expo-router'
+import { View } from 'react-native'
 
 export default function AuthLayout() {
-  const { authState } = useAuth();
+	const { authState } = useAuth()
 
-  if (authState?.authenticated === true) {
-    return <Redirect href={'/'} />;
-  }
+	if (authState?.authenticated === true) {
+		return <Redirect href={'/'} />
+	}
 
-  return (
-    <View style={defaultStyles.container}>
-      <Stack>
-        <Stack.Screen
-          name="login"
-          options={{
-          ...StackScreenWithSearchBar,
-          headerShown: false }}
-        />
-        <Stack.Screen
-          name="register"
-          options={{
-          ...StackScreenWithSearchBar,
-          headerShown: false }} 
-        />
-      </Stack>
-    </View>
-  );
+	return (
+		<View style={defaultStyles.container}>
+			<Stack>
+				<Stack.Screen
+					name="login"
+					options={{
+						headerShown: false,
+						animation: 'none',
+						...StackScreenWithSearchBar,
+					}}
+				/>
+				<Stack.Screen
+					name="register"
+					options={{
+						headerShown: false,
+						animation: 'none',
+						...StackScreenWithSearchBar,
+					}}
+				/>
+			</Stack>
+		</View>
+	)
 }
--- app/app/(auth)/login.tsx
+++ app/app/(auth)/login.tsx
@@ -1,97 +1,130 @@
-import { View, Text, TextInput, StyleSheet, Alert, KeyboardAvoidingView, Platform, useColorScheme } from 'react-native';
-import { useState } from 'react';
-import Button from '@/components/Button';
-import Colors from '@/constants/Colors';
-import { Link } from 'expo-router';
-import { useAuth } from '@/providers/AuthProvider';
+import Button from '@/components/Button'
+import { colors } from '@/constants/tokens'
+import { useAuth } from '@/providers/AuthProvider'
+import { Link } from 'expo-router'
+import { useState } from 'react'
+import {
+	Alert,
+	Image,
+	Keyboard,
+	KeyboardAvoidingView,
+	Platform,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableWithoutFeedback,
+	View,
+} from 'react-native'
 
 const LoginScreen = () => {
-  const colorScheme = useColorScheme();
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
-  const { onLogin } = useAuth();
+	const [login, setLogin] = useState('')
+	const [password, setPassword] = useState('')
+	const [loading, setLoading] = useState(false)
+	const { onLogin } = useAuth()
 
-  async function signIn() {
-    setLoading(true);
-    try {
-      await onLogin(login, password);
-    } catch (error) {
-      Alert.alert("Login Failed", error.message);
-    } finally {
-      setLoading(false);
-    }
-  }
+	async function signIn() {
+		setLoading(true)
+		try {
+			await onLogin(login, password)
+		} catch (error) {
+			Alert.alert('Login Failed', error.message)
+		} finally {
+			setLoading(false)
+		}
+	}
 
-  const dynamicStyles = StyleSheet.create({
-    container: { 
-      padding: 20,
-      justifyContent: 'center',
-      flex: 1,
-      backgroundColor: colorScheme === 'dark' ? Colors.dark.background : Colors.light.background,
-    },
-    label: {
-      color: colorScheme === 'dark' ? Colors.dark.text : Colors.light.text,
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: colorScheme === 'dark' ? Colors.dark.inputBorder : Colors.light.inputBorder,
-      padding: 10,
-      marginTop: 5,
-      marginBottom: 20,
-      backgroundColor: colorScheme === 'dark' ? Colors.dark.inputBackground : Colors.light.inputBackground,
-      borderRadius: 5,
-      fontSize: 18,
-      color: colorScheme === 'dark' ? Colors.dark.text : Colors.light.text,
-    },
-    textButton: {
-      alignSelf: 'center',
-      fontWeight: 'bold',
-      color: colorScheme === 'dark' ? Colors.dark.textSecondary : Colors.light.textSecondary,
-      marginVertical: 10,
-    },
-  });
+	return (
+		<View style={{ flex: 1 }}>
+			<KeyboardAvoidingView
+				style={{ flex: 1 }}
+				behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+			>
+				<View style={{ flex: 1 }}>
+					<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+						<View style={styles.container}>
+							<View>
+								<View style={styles.imageContainer}>
+									<Image source={require('@/assets/icon.png')} style={styles.image} />
+								</View>
+								<Text style={styles.label}>Email or Username</Text>
+								<TextInput
+									value={login}
+									onChangeText={setLogin}
+									placeholder="ElSeniorDeTango"
+									autoCorrect={false}
+									autoComplete="username"
+									autoCapitalize="none"
+									textContentType="emailAddress"
+									keyboardType="email-address"
+									style={styles.input}
+								/>
 
-  return (
-    <View style={{ flex: 1 }}> 
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-      >
-        <View style={dynamicStyles.container}>
-          <Text style={dynamicStyles.label}>Email or Username</Text>
-          <TextInput
-            value={login}
-            onChangeText={setLogin}
-            placeholder='ElSeniorDeTango'
-            autoCorrect={false}
-            autoComplete='off'
-            autoCapitalize='none'
-            textContentType='oneTimeCode'
-            style={dynamicStyles.input}
-          />
+								<Text style={styles.label}>Password</Text>
+								<TextInput
+									value={password}
+									onChangeText={setPassword}
+									placeholder=""
+									style={styles.input}
+									autoComplete="password"
+									secureTextEntry
+								/>
 
-          <Text style={dynamicStyles.label}>Password</Text>
-          <TextInput
-            value={password}
-            onChangeText={setPassword}
-            placeholder=""
-            style={dynamicStyles.input}
-            secureTextEntry
-          />
+								<Button
+									onPress={signIn}
+									disabled={loading}
+									text={loading ? 'Signing in...' : 'Sign in'}
+									style={styles.button}
+								/>
+								<Link replace href="/register" style={styles.textButton}>
+									Create an account
+								</Link>
+							</View>
+						</View>
+					</TouchableWithoutFeedback>
+				</View>
+			</KeyboardAvoidingView>
+		</View>
+	)
+}
 
-          <Button
-            onPress={signIn}
-            disabled={loading}
-            text={loading ? 'Signing in...' : 'Sign in'}
-          />
-          <Link replace href="/register" style={dynamicStyles.textButton}>
-            Create an account
-          </Link>
-        </View>
-      </KeyboardAvoidingView>
-    </View>
-  );
-};
+const styles = StyleSheet.create({
+	container: {
+		padding: 20,
+		flex: 1,
+		justifyContent: 'center',
+	},
+	label: {
+		color: colors.text,
+	},
+	input: {
+		width: '100%',
+		borderWidth: 1,
+		borderColor: colors.icon,
+		padding: 10,
+		marginTop: 5,
+		marginBottom: 20,
+		backgroundColor: colors.background,
+		borderRadius: 5,
+		fontSize: 18,
+		color: colors.text,
+	},
+	textButton: {
+		alignSelf: 'center',
+		fontWeight: 'bold',
+		color: colors.text,
+		marginVertical: 10,
+	},
+	image: {
+		width: 200,
+		height: 200,
+	},
+	imageContainer: {
+		width: '100%',
+		alignItems: 'center',
+	},
+	button: {
+		width: '100%',
+	},
+})
 
-export default LoginScreen;
+export default LoginScreen
