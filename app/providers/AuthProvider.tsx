8109aa4a26507f27a919f7e02d1d96ca7baf7692
Justin Marsh <jmarsh24@gmail.com>
2024-06-15 22:58:40 +0200

    remove console log

--- app/providers/AuthProvider.tsx
+++ app/providers/AuthProvider.tsx
@@ -29,7 +29,7 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
   useEffect(() => {
     const loadToken = async () => {
       const token = await SecureStore.getItemAsync('token');
-      console.log(token);
+
       if (token) {
         setAuthState({
           token: token,
