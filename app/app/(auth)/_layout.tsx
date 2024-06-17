cfff17e3126e21295392fa009d6d29ade1181cd0
Justin Marsh <jmarsh24@gmail.com>
2024-06-17 15:19:13 +0200

    fix

--- app/app/(auth)/_layout.tsx
+++ app/app/(auth)/_layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout() {
 	const { authState } = useAuth()
 
 	if (authState?.authenticated === true) {
-		return <Redirect href={'/'} />
+		return <Redirect href={'/(tabs)/(home)/'} />
 	}
 
 	return (
