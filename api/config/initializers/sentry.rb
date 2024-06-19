0a0f7c38eb31c55908daefb0bccf610517de2f90
Justin Marsh <jmarsh24@gmail.com>
2024-06-19 13:41:36 +0200

    remove logs

--- api/config/initializers/sentry.rb
+++ api/config/initializers/sentry.rb
@@ -9,20 +9,4 @@ Sentry.init do |config|
 
   config.environment = sentry_environment
   config.enabled_environments = [sentry_environment].filter_map(&:presence).excluding("development", "test")
-
-  # Adjust traces sampler to exclude /up transactions
-  config.traces_sampler = lambda do |sampling_context|
-    transaction_context = sampling_context[:transaction_context]
-    op = transaction_context[:op]
-    transaction_name = transaction_context[:name]
-
-    # Exclude /up transactions
-    if op == 'http.server' && transaction_name == '/up'
-      0.0
-    else
-      0.5
-    end
-  end
-
-  config.traces_sample_rate = 1.0 # Ensure this is set as a fallback
 end
