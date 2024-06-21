6e6d81979e60e7caecd11914c08ac685a8ff71e1
Justin Marsh <jmarsh24@gmail.com>
2024-06-21 21:15:48 +0200

    update

--- api/tailwind.config.js
+++ api/tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   content: [
-    "./app/views/**/*.{html,html.slim,erb}",
-    "./app/views/devise/**/*.{html,html.slim,erb}",
-    "./app/frontend/components/**/*.{vue,js,ts,jsx,tsx}",
-    "./app/frontend/**/*.{vue,js,ts,jsx,tsx}",
-    "./app/**/*.{vue,js,ts,jsx,tsx}",
+    "./app/views/**/*.{html,html.erb,erb}",
+    "./app/frontend/components/**/*.{js,ts,jsx,tsx}",
+    "./app/frontend/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
