f3efb41136d58b3d6797eb59f29f349c3704dc8e
Justin Marsh <jmarsh24@gmail.com>
2024-06-21 21:18:22 +0200

    rubocop

--- api/app/helpers/nav_helper.rb
+++ api/app/helpers/nav_helper.rb
@@ -5,8 +5,8 @@ module NavHelper
     url = url_for(options)
     starts_with = html_options.delete(:starts_with)
     html_options[:class] ||= []
-    active_class = html_options.delete(:active_class) || 'is-active'
-    inactive_class = html_options.delete(:inactive_class) || ''
+    active_class = html_options.delete(:active_class) || "is-active"
+    inactive_class = html_options.delete(:inactive_class) || ""
 
     active = Array.wrap(starts_with).any? { |path| request.path.start_with?(path) } || request.path == url
     html_options[:class] << (active ? " #{active_class}" : " #{inactive_class}")
@@ -15,6 +15,4 @@ module NavHelper
 
     link_to(name, url, html_options, &block)
   end
-
-
 end
