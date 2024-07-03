04b5ef27d18826f201969bca485b08ed96d743ef
Justin Marsh <jmarsh24@gmail.com>
2024-07-03 19:48:36 +0200

    working album_art

--- api/app/models/audio_processing/album_art_extractor.rb
+++ api/app/models/audio_processing/album_art_extractor.rb
@@ -1,29 +1,30 @@
 module AudioProcessing
   class AlbumArtExtractor
-    attr_reader :file
-
-    def initialize(file)
+    def initialize(file:)
       @file = file
     end
 
     def extract
-      Tempfile.create(["album_art", ".jpg"]) do |tempfile|
-        movie = FFMPEG::Movie.new(file.path)
+      Tempfile.create(["album-art_", ".jpg"]) do |tempfile|
+        movie = FFMPEG::Movie.new(@file.path)
+
+        if !movie.valid?
+          raise FFMPEG::Error, "Invalid audio file"
+        end
 
         movie.transcode(tempfile.path,
-          {
-            vcodec: "mjpeg",
-            qscale: "1",
-            frame_rate: "1",
-            frames: "1",
-            custom: ["-an"]
-          })
+          vcodec: "mjpeg",
+          qscale: "1",
+          frame_rate: "1",
+          frames: "1",
+          custom: ["-an"])
 
-        if File.exist?(tempfile.path) && !tempfile.size.zero?
-          yield tempfile if block_given?
-        end
+        yield tempfile if block_given?
+
+        tempfile
       end
-    rescue FFMPEG::Error
+    rescue FFMPEG::Error => e
+      Rails.logger.error "Failed to extract album art: #{e.message}"
       nil
     end
   end
--- api/spec/models/audio_processing/album_art_extractor_spec.rb
+++ api/spec/models/audio_processing/album_art_extractor_spec.rb
@@ -1,24 +1,28 @@
 require "rails_helper"
 
-RSpec.describe AudioProcessing::AlbumArtExtractor do
+RSpec.describe AudioProcessing::AlbumArtExtractor, type: :model do
   describe "#extract" do
     context "when the audio file has album art" do
+      let(:file) { File.open(file_fixture("audio/19401008__volver_a_sonar__roberto_rufino__tango.flac")) }
+
       it "extracts the album art from the file" do
-        file = File.open("spec/fixtures/audio/19401008_volver_a_sonar_roberto_rufino_tango_2476.flac")
-        saved_file = nil
-        described_class.new(file).extract do |tempfile|
-          saved_file = tempfile.path
+        album_art_extractor = AudioProcessing::AlbumArtExtractor.new(file:)
+        album_art_extractor.extract do |tempfile|
+          expect(tempfile).not_to be_nil
           expect(File.exist?(tempfile.path)).to be true
           expect(File.size(tempfile.path)).to be > 0
         end
-        expect(File.exist?(saved_file)).to be_falsy
       end
     end
 
     context "when the audio file does not have album art" do
-      it "returns nil if the file does not have album art" do
-        file = File.open("spec/fixtures/audio/19380307_tinta verde_instrumental_tango_no_thumbnail_2760.aif")
-        expect(described_class.new(file).extract).to be_nil
+      let(:file) { File.open(file_fixture("audio/19380307_tinta verde_instrumental_tango_no_thumbnail_2760.aif")) }
+
+      it "returns nil" do
+        album_art_extractor = AudioProcessing::AlbumArtExtractor.new(file:)
+        album_art_extractor.extract do |tempfile|
+          expect(tempfile).to be_nil
+        end
       end
     end
   end
