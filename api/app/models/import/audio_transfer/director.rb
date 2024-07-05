d02b14f0f87e9e0c8a22e2603042b9315762e25d
Justin Marsh <jmarsh24@gmail.com>
2024-07-05 09:52:45 +0200

    add rollback

--- api/app/models/import/audio_transfer/director.rb
+++ api/app/models/import/audio_transfer/director.rb
@@ -7,8 +7,17 @@ module Import
 
       def import(audio_file:)
         audio_file.file.blob.open do |tempfile|
-          audio_file.update!(status: :processing)
-          build_audio_transfer(audio_file:, tempfile:)
+          ActiveRecord::Base.transaction do
+            audio_file.update!(status: :processing)
+            audio_transfer = build_audio_transfer(audio_file:, tempfile:)
+
+            if audio_transfer.save
+              audio_file.update!(status: :completed)
+            else
+              audio_file.update!(status: :failed, error_message: audio_transfer.errors.full_messages.join(", "))
+              raise ActiveRecord::Rollback
+            end
+          end
         end
       rescue => e
         audio_file.update!(status: :failed, error_message: e.message)
@@ -24,7 +33,7 @@ module Import
         album_art = @builder.extract_album_art(file: tempfile)
         compressed_audio = @builder.compress_audio(file: tempfile)
 
-        audio_transfer = @builder.build_and_attach_audio_transfer(
+        @builder.build_and_attach_audio_transfer(
           audio_file:,
           metadata:,
           waveform:,
@@ -32,13 +41,6 @@ module Import
           album_art:,
           compressed_audio:
         )
-
-        if audio_transfer.save
-          audio_file.update!(status: :completed)
-          audio_transfer
-        else
-          audio_file.update!(status: :failed, error_message: audio_transfer.errors.full_messages.join(", "))
-        end
       end
     end
   end
