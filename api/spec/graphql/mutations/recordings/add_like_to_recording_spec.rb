e7bab197cbe3c62e63444224af8db75772708caf
Justin Marsh <jmarsh24@gmail.com>
2024-07-05 15:24:00 +0200

    fix add like

--- api/spec/graphql/mutations/recordings/add_like_to_recording_spec.rb
+++ api/spec/graphql/mutations/recordings/add_like_to_recording_spec.rb
@@ -1,9 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "liked", type: :graph do
-  let(:admin_user) { users(:admin) }
-  let(:normal_user) { users(:normal) }
-  let(:recording) { recordings(:volver_a_sonar) }
+  let(:user) { create(:user) }
+  let(:recording) { create(:recording) }
   let(:mutation) do
     <<~GQL
       mutation AddLikeToRecording($recordingId: ID!) {
@@ -25,17 +24,19 @@ RSpec.describe "liked", type: :graph do
 
   describe "AddLikeToRecording" do
     it "creates a like" do
-      gql(mutation, variables: {recordingId: recording.id}, user: admin_user)
+      gql(mutation, variables: {recordingId: recording.id}, user:)
 
       expect(data.add_like_to_recording.success).to be_truthy
       expect(data.add_like_to_recording.errors).to be_blank
       expect(data.add_like_to_recording.like.likeable_type).to eq("Recording")
       expect(data.add_like_to_recording.like.likeable_id).to eq(recording.id.to_s)
-      expect(data.add_like_to_recording.like.user.id).to eq(admin_user.id.to_s)
+      expect(data.add_like_to_recording.like.user.id).to eq(user.id.to_s)
     end
 
     it "returns error if user alread liked recording" do
-      gql(mutation, variables: {recordingId: recording.id}, user: normal_user)
+      create(:like, likeable: recording, user:)
+
+      gql(mutation, variables: {recordingId: recording.id}, user:)
 
       expect(data.add_like_to_recording.success).to be_falsey
       expect(data.add_like_to_recording.errors).to eq(["User has already liked this"])
