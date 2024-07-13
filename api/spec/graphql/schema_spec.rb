48f9df72fe6059bd8e0f0833b2561f2c37a081a3
Justin Marsh <jmarsh24@gmail.com>
2024-07-13 16:15:26 +0200

    remove schema dump

--- api/spec/graphql/schema_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "GraphQL schema" do
-  it "must be reflected in the .graphql file" do
-    current_defn = TangocloudSchema.to_definition
-    printout_defn = File.read(Rails.root.join("app/graphql/schema.graphql"))
-    assert_equal(current_defn, printout_defn, "Update the printed schema with `bundle exec rake dump_schema`")
-  end
-end
