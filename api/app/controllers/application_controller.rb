98c71901a9008600bc0279a088eb4be5b835c089
jmarsh24 <jmarsh24@gmail.com>
2024-07-16 11:22:01 +0200

    add policies

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
-  include Pundit::Authorization
   before_action :authenticate_user!
   after_action :verify_authorized, unless: :devise_controller?
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  private
-
-  def user_not_authorized(exception)
-    flash[:alert] = I18n.t("pundit.not_authorized")
-    redirect_back(fallback_location: sign_in_path)
-  end
 end
