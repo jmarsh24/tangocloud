10c389732903ba1b4f487c1b00104589db54dbf1
Justin Marsh <jmarsh24@gmail.com>
2024-07-22 12:31:48 +0200

    add dropdown

--- api/app/frontend/controllers/menu_controller.js
+++ api/app/frontend/controllers/menu_controller.js
@@ -1,14 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["toggleable"]
+  static targets = ["toggleable", "button"]
 
   connect() {
     console.log('Hello, Stimulus!')
+    document.addEventListener('click', this.handleClickOutside.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener('click', this.handleClickOutside.bind(this))
   }
 
   toggle() {
     console.log('Toggling')
     this.toggleableTarget.classList.toggle('hidden')
+  }
+
+  handleClickOutside(event) {
+    if (!this.buttonTarget.contains(event.target) && !this.toggleableTarget.contains(event.target)) {
+      this.toggleableTarget.classList.add('hidden')
+    }
   }
 }
