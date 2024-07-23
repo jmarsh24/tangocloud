a03408d2546bd39dba3080c0804e27e10950921b
dogacozen <dogacozen87@gmail.com>
2024-07-23 19:29:03 +0200

    WIP. Some more classes are modified.

--- app/graphql/types/playback_type.rb
+++ app/graphql/types/playback_type.rb
@@ -2,6 +2,7 @@ module Types
   class PlaybackType < Types::BaseObject
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :id, ID, null: false
+    field :duration, Integer, null: false
 
     belongs_to :recording
     belongs_to :user
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,28 +6,13 @@ module Types
     field :provider, String
     field :uid, String
     field :email, String, null: false
-    # field :first_name, String
-    # field :last_name, String
-
-    # field :password_digest, String, null: false
-    # field :password_reset_token, String
-    # field :password_reset_sent_at, GraphQL::Types::ISO8601DateTime
-    # field :remember_created_at, GraphQL::Types::ISO8601DateTime
-    # field :sign_in_count, Integer
-    # field :current_sign_in_at, GraphQL::Types::ISO8601DateTime
-    # field :last_sign_in_at, GraphQL::Types::ISO8601DateTime
-    # field :current_sign_in_ip, String
-    # field :last_sign_in_ip, String
-    # field :confirmation_token, String
-    # field :confirmed_at, GraphQL::Types::ISO8601DateTime
-    # field :confirmation_sent_at, GraphQL::Types::ISO8601DateTime
-    # field :unconfirmed_email, String
 
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
     has_one :user_preference
     has_many :likes, -> { most_recent }
+    has_many :tandas
     has_many :playlists
     has_many :playlist_items
     has_many :taggings
