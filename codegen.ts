2ccfa6ee3a50a126026c5965204b711b0291230f
Justin Marsh <jmarsh24@gmail.com>
2024-07-23 12:01:04 +0200

    work in progress

---
+++ codegen.ts
@@ -0,0 +1,14 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: './schema.gql',
+  documents: ['native/app/**/*.tsx', 'native/app/**/*.ts'],
+  generates: {
+    './native/app/generated/': {
+      preset: 'client',
+      plugins: [],
+    },
+  },
+};
+
+export default config;
