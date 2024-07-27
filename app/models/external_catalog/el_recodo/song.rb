484a5daa968fb644e3253661c1933a16ba62d4e0
Justin Marsh <jmarsh24@gmail.com>
2024-07-27 14:00:13 +0200

    add db

--- app/models/external_catalog/el_recodo/song.rb
+++ app/models/external_catalog/el_recodo/song.rb
@@ -39,3 +39,28 @@ class ExternalCatalog::ElRecodo::Song < ApplicationRecord
     elements.join(" • ")
   end
 end
+
+# == Schema Information
+#
+# Table name: external_catalog_el_recodo_songs
+#
+#  id                                      :uuid             not null, primary key
+#  date                                    :date             not null
+#  ert_number                              :integer          default(0), not null
+#  title                                   :string           not null
+#  style                                   :string
+#  label                                   :string
+#  instrumental                            :boolean          default(TRUE), not null
+#  lyrics                                  :text
+#  lyrics_year                             :integer
+#  search_data                             :string
+#  matrix                                  :string
+#  disk                                    :string
+#  speed                                   :integer
+#  duration                                :integer
+#  synced_at                               :datetime         not null
+#  page_updated_at                         :datetime
+#  external_catalog_el_recodo_orchestra_id :uuid
+#  created_at                              :datetime         not null
+#  updated_at                              :datetime         not null
+#
