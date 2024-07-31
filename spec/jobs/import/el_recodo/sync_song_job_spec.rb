d21238a969fe9d5e02b8bfe8044defe99d5f0682
Justin Marsh <jmarsh24@gmail.com>
2024-07-31 22:01:47 +0200

    remove unnecessary test

--- spec/jobs/import/el_recodo/sync_song_job_spec.rb
+++ spec/jobs/import/el_recodo/sync_song_job_spec.rb
@@ -13,13 +13,5 @@ RSpec.describe ExternalCatalog::ElRecodo::SyncSongJob, type: :job do
       expect(ExternalCatalog::ElRecodo::SongSynchronizer).to have_received(:new)
       expect(song_synchronizer_instance).to have_received(:sync_song).with(ert_number:)
     end
-
-    it "does not raise an error if SongScraper::PageNotFoundError is raised" do
-      ert_number = 123
-      song_synchronizer_instance = instance_double("ExternalCatalog::ElRecodo::SongSynchronizer")
-      allow(ExternalCatalog::ElRecodo::SongSynchronizer).to receive(:new).and_return(song_synchronizer_instance)
-      allow(song_synchronizer_instance).to receive(:sync_song).and_raise(ExternalCatalog::ElRecodo::SongScraper::PageNotFoundError)
-      expect { ExternalCatalog::ElRecodo::SyncSongJob.perform_now(ert_number:) }.not_to raise_error
-    end
   end
 end
