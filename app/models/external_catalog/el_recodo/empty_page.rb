93cadd329e0377f2941619135d20889f026aba05
Justin Marsh <jmarsh24@gmail.com>
2024-08-27 10:25:53 +0200

    annotate

--- app/models/external_catalog/el_recodo/empty_page.rb
+++ app/models/external_catalog/el_recodo/empty_page.rb
@@ -6,7 +6,7 @@ end
 #
 # Table name: external_catalog_el_recodo_empty_pages
 #
-#  id         :bigint           not null, primary key
+#  id         :uuid             not null, primary key
 #  ert_number :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
