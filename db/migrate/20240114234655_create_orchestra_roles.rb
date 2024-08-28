571e558d1989d54f7cf6dc3472c316acbeccc203
Justin Marsh <jmarsh24@gmail.com>
2024-08-28 20:03:42 +0200

    rubocop

--- db/migrate/20240114234655_create_orchestra_roles.rb
+++ db/migrate/20240114234655_create_orchestra_roles.rb
@@ -1,7 +1,7 @@
 class CreateOrchestraRoles < ActiveRecord::Migration[7.1]
   def change
     create_table :orchestra_roles, id: :uuid do |t|
-      t.string :name, null: false, index: { unique: true }
+      t.string :name, null: false, index: {unique: true}
 
       t.timestamps
     end
