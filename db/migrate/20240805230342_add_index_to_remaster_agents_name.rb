73644868e29308303de3ad536d7b82d806a0b821
Justin Marsh <jmarsh24@gmail.com>
2024-08-28 20:24:49 +0200

    remove all extra migrations

--- db/migrate/20240805230342_add_index_to_remaster_agents_name.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToRemasterAgentsName < ActiveRecord::Migration[7.1]
-  def change
-    add_index :remaster_agents, :name, unique: true
-  end
-end
--- db/migrate/20240805232445_add_unique_index_to_genres.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueIndexToGenres < ActiveRecord::Migration[7.1]
-  def change
-    add_index :genres, :name, unique: true
-  end
-end
