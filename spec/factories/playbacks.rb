d31c179f17d2c846c8ebda9e47f957f9b2104488
Justin Marsh <jmarsh24@gmail.com>
2024-08-29 18:37:59 +0200

    annotate

--- spec/factories/playbacks.rb
+++ spec/factories/playbacks.rb
@@ -4,3 +4,15 @@ FactoryBot.define do
     association :user
   end
 end
+
+# == Schema Information
+#
+# Table name: playbacks
+#
+#  id           :uuid             not null, primary key
+#  duration     :integer          default(0), not null
+#  user_id      :uuid             not null
+#  recording_id :uuid             not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
