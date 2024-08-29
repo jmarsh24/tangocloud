4aff2a4ca93aab14f1537cb233c44c433cbcd7a4
Justin Marsh <jmarsh24@gmail.com>
2024-08-29 21:54:54 +0200

    permit username

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -21,7 +21,7 @@ module Api
     private
 
     def user_params
-      params.require(:user).permit(:email, :password)
+      params.require(:user).permit(:username, :email, :password)
     end
   end
 end
