7064ad560fde023433aeebc623079d116dad6f40
Justin Marsh <jmarsh24@gmail.com>
2024-08-29 11:08:50 +0200

    update you screen

---
+++ native/graphql/queries/user_profile.ts
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client';
+
+export const USER_PROFILE = gql`
+	query userProfile {
+		currentUser {
+			id
+			username
+			userPreference {
+				avatar {
+					blob {
+						url
+					}
+				}
+			}
+			playbacks {
+				edges {
+					node {
+						recording {
+							id
+							composition {
+								title
+							}
+							genre {
+								name
+							}
+							year
+							orchestra {
+								name
+							}
+							digitalRemasters {
+								edges {
+									node {
+										duration
+										album {
+											albumArt {
+												blob {
+													url
+												}
+											}
+										}
+										audioVariants {
+											edges {
+												node {
+													audioFile {
+														blob {
+															url
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`;
