a4043a17522b1de321a9ef7618ca0a2ffe5349c7
Justin Marsh <jmarsh24@gmail.com>
2024-08-31 16:58:16 +0200

    rubocop

--- db/seeds/04_orchestra_periods.rb
+++ db/seeds/04_orchestra_periods.rb
@@ -1,10 +1,10 @@
 pugliese = Orchestra.find_by(name: "Osvaldo Pugliese")
 pugliese_orchestra_periods = [
-  {name: "Early Pugliese", start_date: Date.new(1943,1,1), end_date: Date.new(1947,1,1)},
-  {name: "Finding His Style", start_date: Date.new(1946,1,1), end_date: Date.new(1950,1,1)},
-  {name: "Mature Style", start_date: Date.new(1950,1,1), end_date: Date.new(1955,1,1)},
-  {name: "Maciel Period", start_date: Date.new(1955,1,1), end_date: Date.new(1968,1,1)},
-  {name: "Late Pugliese", start_date: Date.new(1968,1,1), end_date: Date.new(1987,1,1)},
+  {name: "Early Pugliese", start_date: Date.new(1943, 1, 1), end_date: Date.new(1947, 1, 1)},
+  {name: "Finding His Style", start_date: Date.new(1946, 1, 1), end_date: Date.new(1950, 1, 1)},
+  {name: "Mature Style", start_date: Date.new(1950, 1, 1), end_date: Date.new(1955, 1, 1)},
+  {name: "Maciel Period", start_date: Date.new(1955, 1, 1), end_date: Date.new(1968, 1, 1)},
+  {name: "Late Pugliese", start_date: Date.new(1968, 1, 1), end_date: Date.new(1987, 1, 1)}
 ]
 
 pugliese_orchestra_periods.each do |period|
@@ -13,14 +13,14 @@ end
 
 darienzo = Orchestra.find_by(name: "Juan D'Arienzo")
 darienzo_orchestra_periods = [
-  {name: "Sexteto", start_date: Date.new(1928,1,1), end_date: Date.new(1930,1,1)},
-  {name: "With Biagi", start_date: Date.new(1936,1,1), end_date: Date.new(1939,1,1)},
-  {name: "Fast Tempo With Polito", start_date: Date.new(1939,1,1), end_date: Date.new(1940,1,1)},
-  {name: "Slowing Down", start_date: Date.new(1940,1,1), end_date: Date.new(1945,1,1)},
-  {name: "D'Arienzo Is Back", start_date: Date.new(1945,1,1), end_date: Date.new(1951,1,1)},
-  {name: "Salamanca After 50", start_date: Date.new(1950,1,1), end_date: Date.new(1957,1,1)},
-  {name: "Polito Is Back", start_date: Date.new(1957,1,1), end_date: Date.new(1961,1,1)},
-  {name: "Late D'Arienzo", start_date: Date.new(1961,1,1), end_date: Date.new(1976,1,1)},
+  {name: "Sexteto", start_date: Date.new(1928, 1, 1), end_date: Date.new(1930, 1, 1)},
+  {name: "With Biagi", start_date: Date.new(1936, 1, 1), end_date: Date.new(1939, 1, 1)},
+  {name: "Fast Tempo With Polito", start_date: Date.new(1939, 1, 1), end_date: Date.new(1940, 1, 1)},
+  {name: "Slowing Down", start_date: Date.new(1940, 1, 1), end_date: Date.new(1945, 1, 1)},
+  {name: "D'Arienzo Is Back", start_date: Date.new(1945, 1, 1), end_date: Date.new(1951, 1, 1)},
+  {name: "Salamanca After 50", start_date: Date.new(1950, 1, 1), end_date: Date.new(1957, 1, 1)},
+  {name: "Polito Is Back", start_date: Date.new(1957, 1, 1), end_date: Date.new(1961, 1, 1)},
+  {name: "Late D'Arienzo", start_date: Date.new(1961, 1, 1), end_date: Date.new(1976, 1, 1)}
 ]
 
 darienzo_orchestra_periods.each do |period|
@@ -29,10 +29,10 @@ end
 
 troilo = Orchestra.find_by(name: "Aníbal Troilo")
 troilo_orchestra_periods = [
-  {name: "Energetic Period", start_date: Date.new(1938,1,1), end_date: Date.new(1942,1,1)},
-  {name: "Sentimental Golden Age", start_date: Date.new(1942,1,1), end_date: Date.new(1950,1,1)},
-  {name: "TK Years", start_date: Date.new(1950,1,1), end_date: Date.new(1957,1,1)},
-  {name: "Late Troilo", start_date: Date.new(1957,1,1), end_date: Date.new(1969,1,1)},
+  {name: "Energetic Period", start_date: Date.new(1938, 1, 1), end_date: Date.new(1942, 1, 1)},
+  {name: "Sentimental Golden Age", start_date: Date.new(1942, 1, 1), end_date: Date.new(1950, 1, 1)},
+  {name: "TK Years", start_date: Date.new(1950, 1, 1), end_date: Date.new(1957, 1, 1)},
+  {name: "Late Troilo", start_date: Date.new(1957, 1, 1), end_date: Date.new(1969, 1, 1)}
 ]
 
 troilo_orchestra_periods.each do |period|
@@ -41,11 +41,11 @@ end
 
 disarli = Orchestra.find_by(name: "Carlos Di Sarli")
 disarli_orchestra_periods = [
-  {name: "Sexteto", start_date: Date.new(1928,1,1), end_date: Date.new(1932,1,1)},
-  {name: "Energetic Period", start_date: Date.new(1939,1,1), end_date: Date.new(1942,1,1)},
-  {name: "Melodic", start_date: Date.new(1942,1,1), end_date: Date.new(1948,1,1)},
-  {name: "Early 50s", start_date: Date.new(1951,1,1), end_date: Date.new(1956,1,1)},
-  {name: "Late 50s", start_date: Date.new(1956,1,1), end_date: Date.new(1959,1,1)},
+  {name: "Sexteto", start_date: Date.new(1928, 1, 1), end_date: Date.new(1932, 1, 1)},
+  {name: "Energetic Period", start_date: Date.new(1939, 1, 1), end_date: Date.new(1942, 1, 1)},
+  {name: "Melodic", start_date: Date.new(1942, 1, 1), end_date: Date.new(1948, 1, 1)},
+  {name: "Early 50s", start_date: Date.new(1951, 1, 1), end_date: Date.new(1956, 1, 1)},
+  {name: "Late 50s", start_date: Date.new(1956, 1, 1), end_date: Date.new(1959, 1, 1)}
 ]
 
 disarli_orchestra_periods.each do |period|
