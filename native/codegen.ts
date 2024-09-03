dca9bd15820c9383f542392dc6e3a6ea8abcdbba
Justin Marsh <jmarsh24@gmail.com>
2024-09-03 09:49:17 +0200

    codegen

--- native/codegen.ts
+++ native/codegen.ts
@@ -2,11 +2,19 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   schema: '../schema.gql',
+  documents: './src/graphql/**/*.ts',
   generates: {
-    './src/graphql/__generated__/': {
+    '../native/src/graphql/__generated__/': {
       preset: 'client',
-      plugins: [],
+      plugins: [
+        'typescript',  // Generate TypeScript types for the schema
+        'typescript-operations',  // Generate TypeScript types for the GraphQL operations (queries, mutations, etc.)
+        'typescript-react-apollo',  // Generate Apollo hooks for React (React Native)
+      ],
     },
+  },
+  hooks: {
+    afterAllFileWrite: ['prettier --write'],
   },
 };
 
