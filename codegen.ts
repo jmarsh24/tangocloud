4259d4dca9111d4014ecba315527058b84e0c12c
Justin Marsh <jmarsh24@gmail.com>
2024-09-03 10:00:45 +0200

    move back to app

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   schema: './schema.gql',
-  documents: ['native/src/**/*.tsx', 'native/graphql/**/*.ts'],
+  documents: ['native/app/**/*.tsx', 'native/graphql/**/*.ts'],
   generates: {
     './native/src/graphql/__generated__/': {
       preset: 'client',
