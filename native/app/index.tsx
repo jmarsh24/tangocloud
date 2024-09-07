28b727f1cf822eee9d28d514393402d027969bd1
Justin Marsh <jmarsh24@gmail.com>
2024-09-07 21:31:21 +0200

    working auth

--- native/app/index.tsx
+++ native/app/index.tsx
@@ -1,10 +1,25 @@
 import { useAuth } from "@/providers/AuthProvider";
 import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
 
 const Page = () => {
   const { authState } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
 
-  if (authState.authenticated) return <Redirect href="/(root)/(tabs)/home" />;
+  useEffect(() => {
+    if (authState.authenticated !== null) {
+      setIsLoading(false);
+    }
+  }, [authState]);
+
+  if (isLoading) {
+    return <View><Text>Loading...</Text></View>;
+  }
+
+  if (authState.authenticated === true) {
+    return <Redirect href="/(root)/(tabs)" />;
+  }
 
   return <Redirect href="/(auth)/welcome" />;
 };
