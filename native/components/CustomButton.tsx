b5c069ff84c619d8556d3e9329bbef1a544be164
Justin Marsh <jmarsh24@gmail.com>
2024-09-08 14:29:24 +0200

    remove drop shadow on button

--- native/components/CustomButton.tsx
+++ native/components/CustomButton.tsx
@@ -45,7 +45,7 @@ const CustomButton = ({
   return (
     <TouchableOpacity
       onPress={onPress}
-      className={`w-full rounded-full p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(bgVariant)} ${className}`}
+      className={`w-full rounded-full p-3 flex flex-row justify-center items-center ${getBgVariantStyle(bgVariant)} ${className}`}
       {...props}
     >
       {IconLeft && <IconLeft />}
