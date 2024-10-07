629fbf74d9d6c3faf495b777d9b62084d356b6c0
Justin Marsh <jmarsh24@gmail.com>
2024-10-07 22:00:26 +0200

    fix init

--- config/init.sql
+++ config/init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:021022471133130b7a82b74f5aed718b69396026e1d7a839ed4cfdeae4ba4198
-size 162
+oid sha256:1660a539acc2eb854f46a71905db3cd32973f655ed1ff00cef27623a5d40e3ec
+size 129
