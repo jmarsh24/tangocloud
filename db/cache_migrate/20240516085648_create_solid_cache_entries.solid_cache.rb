b42722927938ce74506207e6aa7f7f6d7e20850a
Justin Marsh <jmarsh24@gmail.com>
2024-10-09 11:51:12 +0200

    simplify database.yml

--- db/cache_migrate/20240516085648_create_solid_cache_entries.solid_cache.rb
+++ db/cache_migrate/20240516085648_create_solid_cache_entries.solid_cache.rb
@@ -1,12 +1,15 @@
 # This migration comes from solid_cache (originally 20230724121448)
 class CreateSolidCacheEntries < ActiveRecord::Migration[7.0]
   def change
-    create_table :solid_cache_entries do |t|
-      t.binary :key, null: false, limit: 1024
-      t.binary :value, null: false, limit: 512.megabytes
-      t.datetime :created_at, null: false
-
-      t.index :key, unique: true
+    create_table :solid_cache_entries, force: :cascade do |t|
+      t.binary "key", null: false
+      t.binary "value", null: false
+      t.datetime "created_at", null: false
+      t.bigint "key_hash", null: false
+      t.integer "byte_size", null: false
+      t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+      t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+      t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
     end
   end
 end
