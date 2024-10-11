aa91b5c98d79b1cc8a2c57a8784604485310e8b3
Justin Marsh <jmarsh24@gmail.com>
2024-10-11 14:25:54 +0200

    remove rails storage proxy initializer

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,4 +1,3 @@
-Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
 Rails.application.config.active_storage.content_types_allowed_inline = [
   "audio/flac",
   "audio/x-aiff",
