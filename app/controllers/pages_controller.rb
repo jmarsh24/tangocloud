71181fb0b7072a0dbfec1aa893e0a8c2f1a50d89
Justin Marsh <jmarsh24@gmail.com>
2024-10-17 01:31:39 +0200

    add view component ui

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,8 @@
 class PagesController < ApplicationController
-  layout "marketing"
   skip_after_action :verify_authorized, :verify_policy_scoped
   skip_before_action :authenticate_user!
 
   def landing
-    flash[:notice] = "Welcome to the landing page"
   end
 
   def privacy
