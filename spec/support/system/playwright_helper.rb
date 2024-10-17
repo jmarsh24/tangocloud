a372843e97db72873d53eba389b132bcf6fefcda
Justin Marsh <jmarsh24@gmail.com>
2024-10-17 21:30:01 +0200

    working login and registration specs

--- spec/support/system/playwright_helper.rb
+++ spec/support/system/playwright_helper.rb
@@ -1,4 +1,4 @@
-module CupriteHelper
+module PlaywrightHelper
   def pause
     Capybara.current_session.driver.with_playwright_page do |page|
       page.context.enable_debug_console!
@@ -7,7 +7,7 @@ module CupriteHelper
   end
 
   def screenshot(name)
-    return unless Rails.application.credentials.dig(:screenshot)
+    return unless ENV["SCREENSHOTS"]
 
     width = page.current_window.size[0]
     height = page.current_window.size[1]
@@ -19,5 +19,17 @@ module CupriteHelper
 end
 
 RSpec.configure do |config|
-  config.include CupriteHelper, type: :system
+  config.include PlaywrightHelper, type: :system
+  config.before(:each, type: :system) do |example|
+    page.driver.start_tracing(title: example.metadata[:full_description], screenshots: true, snapshots: true)
+  end
+  config.after(:each, type: :system) do |example|
+    if example.exception
+      path = Rails.root.join("tmp/playwright/traces/#{example.metadata[:full_description].parameterize}.zip")
+      page.driver.stop_tracing(path:)
+      puts "View failed trace: npx playwright show-trace #{path}"
+    else
+      page.driver.stop_tracing
+    end
+  end
 end
