d6b89eea2c5a10b15c8fbd86ed3b3de35235abb4
Justin Marsh <jmarsh24@gmail.com>
2024-10-17 18:30:10 +0200

    force dark mde

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'class', // Ensure dark mode is based on a class
   content: [
     './app/views/**/*.html.erb',
     './app/components/**/*',
@@ -14,6 +15,9 @@ module.exports = {
     },
   },
   plugins: [
-    require("daisyui"),
-    ]
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ['dark'],
+  },
 };
