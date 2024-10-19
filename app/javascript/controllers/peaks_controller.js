a75c4dc5f9f6d3352286b2936e5646fd882431de
Justin Marsh <jmarsh24@gmail.com>
2024-10-19 14:42:10 +0200

    fix waveform?

--- app/javascript/controllers/peaks_controller.js
+++ app/javascript/controllers/peaks_controller.js
@@ -32,18 +32,9 @@ export default class extends Controller {
       const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
       gradient.addColorStop(0, "#656666");
       gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666");
-      gradient.addColorStop(
-        (canvas.height * 0.7 + 1) / canvas.height,
-        "#ffffff"
-      );
-      gradient.addColorStop(
-        (canvas.height * 0.7 + 2) / canvas.height,
-
-      );
-      gradient.addColorStop(
-        (canvas.height * 0.7 + 3) / canvas.height,
-        "#B1B1B1"
-      );
+      gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, "#ffffff");
+      gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, "#B1B1B1");
+      gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, "#B1B1B1");
       gradient.addColorStop(1, "#B1B1B1");
 
       // Define progress gradient
