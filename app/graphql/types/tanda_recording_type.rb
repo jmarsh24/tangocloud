5db8604fa79cc094528cbfd94d039f741dde7714
Justin Marsh <jmarsh24@gmail.com>
2024-10-20 15:40:23 +0200

    rubocop

--- app/graphql/types/tanda_recording_type.rb
+++ app/graphql/types/tanda_recording_type.rb
@@ -1,9 +1,9 @@
 # app/graphql/types/tanda_recording_type.rb
 module Types
   class TandaRecordingType < Types::BaseObject
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :id, ID, null: false
     field :position, Integer, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
     belongs_to :tanda, null: false
