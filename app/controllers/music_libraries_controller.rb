ef544d1ea83041263e82751be4008c9142d645b9
Justin Marsh <jmarsh24@gmail.com>
2024-10-20 22:48:42 +0200

    fix n+1

--- app/controllers/music_libraries_controller.rb
+++ app/controllers/music_libraries_controller.rb
@@ -4,9 +4,9 @@ class MusicLibrariesController < ApplicationController
   def show
     authorize :music_library, :show?
 
-    @recordings = policy_scope(Recording).random.limit(36)
-    @playlists = policy_scope(Playlist).limit(36).shuffle
-    @tandas = policy_scope(Tanda).limit(36)
-    @orchestras = policy_scope(Orchestra).ordered_by_recordings.limit(36)
+    @recordings = policy_scope(Recording).random.limit(36).includes(:composition, :orchestra, :singers, :genre, digital_remasters: [audio_variants: [audio_file_attachment: :blob], album: [album_art_attachment: :blob]])
+    @playlists = policy_scope(Playlist).limit(36).with_attached_image.shuffle
+    @tandas = policy_scope(Tanda).limit(36).with_attached_image.shuffle
+    @orchestras = policy_scope(Orchestra).ordered_by_recordings.limit(36).with_attached_image
   end
 end
