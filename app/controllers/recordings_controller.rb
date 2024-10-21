17b8980363479715afaecee0bac13927f316f585
Justin Marsh <jmarsh24@gmail.com>
2024-10-21 22:01:12 +0200

    add some styling changes

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -1,6 +1,6 @@
 class RecordingsController < ApplicationController
   def index
-    @recordings = policy_scope(Recording).limit(100).includes(:composition, :orchestra, :singers, :genre, digital_remasters: [audio_variants: [audio_file_attachment: :blob], album: [album_art_attachment: :blob]])
+    @recordings = policy_scope(Recording).random.limit(100).includes(:composition, :orchestra, :singers, :genre, digital_remasters: [audio_variants: [audio_file_attachment: :blob], album: [album_art_attachment: :blob]])
 
     authorize Recording
   end
