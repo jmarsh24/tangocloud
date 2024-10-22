a6c7356e1f0a190c8a24a431dd51d8fb40c0abfc
Justin Marsh <jmarsh24@gmail.com>
2024-10-23 01:56:47 +0200

    add tanda policy back

---
+++ app/policies/tanda_policy.rb
@@ -0,0 +1,16 @@
+class TandaPolicy < ApplicationPolicy
+  def index?
+    user.admin? || user.tester? || user.editor?
+  end
+
+  def show?
+    user.admin? || user.tester? || user.editor?
+  end
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+end
