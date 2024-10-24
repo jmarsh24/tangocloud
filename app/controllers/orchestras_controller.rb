22002a0fbbaeec46ecb915d0e864d52c98199386
Justin Marsh <jmarsh24@gmail.com>
2024-10-24 02:48:19 +0200

    things working expect orchestra_periods

--- app/controllers/orchestras_controller.rb
+++ app/controllers/orchestras_controller.rb
@@ -1,37 +1,44 @@
 class OrchestrasController < ApplicationController
-  def index
-    @orchestras = policy_scope(Orchestra.ordered_by_recordings).limit(100).with_attached_image
-
-    authorize Orchestra
-  end
-
   def show
-    @orchestra = policy_scope(Orchestra.includes(:genres, recordings: [:composition, digital_remasters: [album: [album_art_attachment: :blob]]])).friendly.find(params[:id])
+    @orchestra = policy_scope(
+      Orchestra.includes(
+        :genres,
+        recordings: [
+          :composition,
+          digital_remasters: [album: [album_art_attachment: :blob]]
+        ]
+      )
+    ).friendly.find(params[:id])
     authorize @orchestra
 
-    filters = params.slice(:year, :genre, :orchestra_period, :singer)
+    @filters = params.permit(:year, :genre, :orchestra_period, :singer).to_h
 
     @recordings = @orchestra.recordings.with_associations
 
-    if filters[:year].present?
-      @recordings = @recordings.where("EXTRACT(YEAR FROM recorded_date) = ?", filters[:year])
+    if @filters[:year].present?
+      @recordings = @recordings.where("EXTRACT(YEAR FROM recorded_date) = ?", @filters[:year])
     end
 
-    if filters[:genre].present?
-      @recordings = @recordings.joins(:genres).where(genres: {name: filters[:genre]})
+    if @filters[:genre].present?
+      @recordings = @recordings.joins(:genres).where(genres: {name: @filters[:genre]})
     end
 
-    if filters[:orchestra_period].present?
-      @recordings = @recordings.joins(:orchestra).where(orchestra: {orchestra_periods: {name: filters[:orchestra_period]}})
+    if @filters[:orchestra_period].present?
+      @recordings = @recordings
+        .joins(orchestra: :orchestra_periods)
+        .where(orchestra_periods: {name: @filters[:orchestra_period]})
     end
 
-    if filters[:singer].present?
-      @recordings = @recordings.joins(recording_singers: :person).where(people: {name: filters[:singer]})
+    if @filters[:singer].present?
+      @recordings = @recordings
+        .joins(recording_singers: :person)
+        .where(people: {name: @filters[:singer]})
     end
 
     recording_ids = @recordings.select(:id)
 
-    @years = @recordings
+    @years = @orchestra.recordings
+      .where(id: recording_ids)
       .where.not(recorded_date: nil)
       .distinct
       .pluck(Arel.sql("EXTRACT(YEAR FROM recorded_date)"))
@@ -43,12 +50,12 @@ class OrchestrasController < ApplicationController
       .where(recordings: {id: recording_ids})
       .group("genres.id")
       .order(Arel.sql("CASE
-        WHEN genres.name = 'Tango' THEN 1
-        WHEN genres.name = 'Milonga' THEN 2
-        WHEN genres.name = 'Vals' THEN 3
-        WHEN genres.name = 'Condombe' THEN 4
-        ELSE 5
-      END, genres.name ASC"))
+                  WHEN genres.name = 'Tango' THEN 1
+                  WHEN genres.name = 'Milonga' THEN 2
+                  WHEN genres.name = 'Vals' THEN 3
+                  WHEN genres.name = 'Condombe' THEN 4
+                  ELSE 5
+                END, genres.name ASC"))
 
     @orchestra_periods = OrchestraPeriod
       .joins(orchestra: :recordings)
@@ -63,7 +70,6 @@ class OrchestrasController < ApplicationController
       .select("people.*, COUNT(recording_singers.recording_id) AS recording_count")
       .order("recording_count DESC")
 
-    # @recordings = @recordings.page(params[:page]).per(20)
     @recordings = @recordings.limit(40)
   end
 end
