7b91f3e7f125e21c9b197c779ae182d0d859f583
Justin Marsh <jmarsh24@gmail.com>
2024-10-29 11:48:57 +0100

    add the ability to hide/show passwords

--- app/javascript/controllers/password_visibility_controller.js
+++ app/javascript/controllers/password_visibility_controller.js
@@ -3,19 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="password-visibility"
 export default class extends Controller {
   static targets = ["password", "eye", "eyeSlash"]
-  connect() {
-    console.log("Connected to password visibility controller")
-  }
+
 
   toggle() {
-    if (this.passwordTarget.type === "password") {
-      this.passwordTarget.type = "text"
-      this.eyeTarget.classList.add("hidden")
-      this.eyeSlashTarget.classList.remove("hidden")
-    } else {
-      this.passwordTarget.type = "password"
-      this.eyeTarget.classList.remove("hidden")
-      this.eyeSlashTarget.classList.add("hidden")
-    }
+    const newType = this.passwordTargets[0].type === "password" ? "text" : "password"
+    this.setPasswordTypes(newType)
+    this.toggleIcons(newType)
+  }
+
+  setPasswordTypes(type) {
+    this.passwordTargets.forEach(passwordField => {
+      passwordField.type = type
+    })
+  }
+
+  toggleIcons(type) {
+    const isPasswordVisible = type === "text"
+    this.eyeTargets.forEach(eyeIcon => eyeIcon.classList.toggle("hidden", isPasswordVisible))
+    this.eyeSlashTargets.forEach(eyeSlashIcon => eyeSlashIcon.classList.toggle("hidden", !isPasswordVisible))
   }
 }
