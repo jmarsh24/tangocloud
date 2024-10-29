794dd1f3f5fb42c3013423973bc372db130da26f
Justin Marsh <jmarsh24@gmail.com>
2024-10-29 13:48:29 +0100

    remove recordings

--- app/controllers/recordings_controller.rb
+++
@@ -1,7 +0,0 @@
-class RecordingsController < ApplicationController
-  def index
-    @recordings = policy_scope(Recording).random.limit(100).strict_loading.includes(:composition, :orchestra, :singers, :genre, digital_remasters: [audio_variants: [audio_file_attachment: :blob], album: [album_art_attachment: :blob]])
-
-    authorize Recording
-  end
-end
