56931aa22c1b8b9d8de55515df46354206348334
Justin Marsh <jmarsh24@gmail.com>
2024-10-30 22:37:40 +0100

    fix includes

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -1,6 +1,9 @@
 class RecordingsController < ApplicationController
   def show
-    @recording = policy_scope(Recording).with_associations.friendly.find(params[:id])
+    @recording = policy_scope(Recording)
+      .with_associations
+      .includes(composition: {composition_lyrics: :lyric})
+      .friendly.find(params[:id])
     authorize @recording
   end
 end
