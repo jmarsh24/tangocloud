803191d224b05f0314fd4e37e2905d7dd73af7fb
Justin Marsh <jmarsh24@gmail.com>
2024-10-31 00:27:16 +0100

    add wave surfer (#270)
    * initial commit for add wave surfer
    
    * working
    
    * remove mobile
    
    * music player

--- app/javascript/controllers/music_player_controller.js
+++ app/javascript/controllers/music_player_controller.js
@@ -1,10 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
 import WaveSurfer from "wavesurfer.js";
-import RegionsPlugin from "wavesurfer.js/dist/plugins/regions.esm.js";
 
 export default class extends Controller {
   static targets = [
-    "audio",
     "container",
     "playButton",
     "markerDescription",
@@ -13,153 +11,123 @@ export default class extends Controller {
     "playIcon",
     "pauseIcon",
     "hover",
+    "loadingIndicator",
   ];
+
   static values = {
-    markers: { type: Array, default: [] },
     playing: { type: Boolean, default: false },
+    audioUrl: String,
   };
 
-  initialize() {
-    requestAnimationFrame(() => {
-      const canvas = document.createElement("canvas");
-      canvas.height = 100;
-      const ctx = canvas.getContext("2d");
-
-      const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
-      gradient.addColorStop(0, "#656666");
-      gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666");
-      gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, "#ffffff");
-      gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, "#B1B1B1");
-      gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, "#B1B1B1");
-      gradient.addColorStop(1, "#B1B1B1");
-
-      const progressGradient = ctx.createLinearGradient(
-        0,
-        0,
-        0,
-        canvas.height * 1.35
-      );
-      progressGradient.addColorStop(0, "#EE772F");
-      progressGradient.addColorStop(
-        (canvas.height * 0.7) / canvas.height,
-        "#EB4926"
-      );
-      progressGradient.addColorStop(
-        (canvas.height * 0.7 + 1) / canvas.height,
-        "#ffffff"
-      );
-      progressGradient.addColorStop(
-        (canvas.height * 0.7 + 2) / canvas.height,
-        "#ffffff"
-      );
-      progressGradient.addColorStop(
-        (canvas.height * 0.7 + 3) / canvas.height,
-        "#F6B094"
-      );
-      progressGradient.addColorStop(1, "#F6B094");
-
-      this.wavesurfer = WaveSurfer.create({
-        container: this.containerTarget,
-        waveColor: gradient,
-        progressColor: progressGradient,
-        media: this.audioTarget,
-        barWidth: 2,
-        barRadius: 2,
-        barGap: 1,
-        hideScrollbar: true,
-        backend: 'MediaElement'
-      });
-
-      this.regions = this.wavesurfer.registerPlugin(RegionsPlugin.create());
-
-      this.wavesurfer.on("play", () => {
-        this.playingValue = true;
-      });
-
-      this.wavesurfer.on("pause", () => {
-        this.playingValue = false;
-      });
-
-      this.wavesurfer.on("finish", () => {
-        this.playingValue = false;
-      });
-
-      this.wavesurfer.on("decode", (duration) => {
-        this.durationTarget.textContent = this.formatTime(duration);
-      });
-
-      this.wavesurfer.on("timeupdate", (currentTime) => {
-        this.timeTarget.textContent = this.formatTime(currentTime);
-      });
-    });
-  }
-
   connect() {
-  }
+    if (!this.hasAudioUrlValue) return;
 
-  handleHover = (e) => {
-    this.hoverTarget.style.width = `${e.offsetX}px`;
+    this.createGradients();
+    this.initializeWaveSurfer();
+    this.setupEventListeners();
   }
 
   disconnect() {
-    this.wavesurfer.destroy();
-    this.regions.destroy();
+    this.wavesurfer?.destroy();
   }
 
   playPause() {
-    this.wavesurfer.playPause();
+    this.wavesurfer?.playPause();
   }
+
+  handleHover = (e) => {
+    if (this.hasHoverTarget) {
+      this.hoverTarget.style.width = `${e.offsetX}px`;
+    }
+  };
 
   playingValueChanged() {
-    const playIcon = this.playIconTarget
-    const pauseIcon = this.pauseIconTarget
-
-    if (this.playingValue) {
-      playIcon.classList.add('hidden');
-      pauseIcon.classList.remove('hidden');
-    } else {
-      playIcon.classList.remove('hidden');
-      pauseIcon.classList.add('hidden');
+    if (this.hasPlayIconTarget && this.hasPauseIconTarget) {
+      this.playIconTarget.classList.toggle("hidden", this.playingValue);
+      this.pauseIconTarget.classList.toggle("hidden", !this.playingValue);
     }
-  }
-
-  markersValueChanged(markers, previousMarkers) {
-    const addedMarkers = markers.filter(
-      (marker) =>
-        !new Set(previousMarkers.map((prevMarker) => prevMarker.time)).has(
-          marker.time
-        )
-    );
-
-    addedMarkers?.forEach((m) => this.#handleAddition(m));
-  }
-
-  addMarkerAtCurrentTime() {
-    this.markersValue = [
-      ...this.markersValue,
-      {
-        time: this.wavesurfer.getCurrentTime(),
-        description: this.markerDescriptionTarget.value,
-      },
-    ];
-
-    this.markerDescriptionTarget.value = "";
-  }
-
-  #handleAddition(value) {
-    this.regions?.addRegion({
-      start: value.time,
-      content: value.description,
-      color: "rgba(255, 0, 0, 0.5)",
-      drag: false,
-      resize: false,
-    });
   }
 
   formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
-    const secondsRemainder = Math.round(seconds) % 60;
-    const paddedSeconds = `0${secondsRemainder}`.slice(-2);
-    return `${minutes}:${paddedSeconds}`;
+    const secondsRemainder = Math.round(seconds % 60);
+    return `${minutes}:${secondsRemainder.toString().padStart(2, "0")}`;
   }
+
+  createGradients() {
+    const canvasHeight = this.containerTarget.offsetHeight || 100;
+    const canvas = document.createElement("canvas");
+    canvas.height = canvasHeight;
+    const ctx = canvas.getContext("2d");
+    const heightFactor = canvasHeight * 1.35;
+    const stopPosition1 = (canvasHeight * 0.7) / canvasHeight;
+    const stopPosition2 = (canvasHeight * 0.7 + 1) / canvasHeight;
+
+    this.waveGradient = ctx.createLinearGradient(0, 0, 0, heightFactor);
+    this.waveGradient.addColorStop(0, "#656666");
+    this.waveGradient.addColorStop(stopPosition1, "#656666");
+    this.waveGradient.addColorStop(stopPosition2, "#ffffff");
+    this.waveGradient.addColorStop(1, "#B1B1B1");
+
+    this.progressGradient = ctx.createLinearGradient(0, 0, 0, heightFactor);
+    this.progressGradient.addColorStop(0, "#EE772F");
+    this.progressGradient.addColorStop(stopPosition1, "#EB4926");
+    this.progressGradient.addColorStop(stopPosition2, "#ffffff");
+    this.progressGradient.addColorStop(1, "#F6B094");
+  }
+
+  initializeWaveSurfer() {
+    this.wavesurfer = WaveSurfer.create({
+      container: this.containerTarget,
+      waveColor: this.waveGradient,
+      progressColor: this.progressGradient,
+      url: this.audioUrlValue,
+      barWidth: 2,
+      barRadius: 2,
+      barGap: 1,
+      responsive: true,
+    });
+  }
+
+  setupEventListeners() {
+    this.wavesurfer.on("play", this.onPlay);
+    this.wavesurfer.on("pause", this.onPause);
+    this.wavesurfer.on("finish", this.onFinish);
+    this.wavesurfer.on("decode", this.onDecode);
+    this.wavesurfer.on("ready", this.hideLoadingIndicator);
+    this.wavesurfer.on("audioprocess", this.showLoadingIndicator);
+    this.wavesurfer.on("timeupdate", this.onTimeUpdate);
+  }
+
+  onPlay = () => {
+    this.playingValue = true;
+  };
+
+  onPause = () => {
+    this.playingValue = false;
+  };
+
+  onFinish = () => {
+    this.playingValue = false;
+  };
+
+  onDecode = (duration) => {
+    this.durationTarget.textContent = this.formatTime(duration);
+  };
+
+  onTimeUpdate = (currentTime) => {
+    this.timeTarget.textContent = this.formatTime(currentTime);
+  };
+
+  showLoadingIndicator = () => {
+    if (this.hasLoadingIndicatorTarget) {
+      this.loadingIndicatorTarget.classList.remove("hidden");
+    }
+  };
+
+  hideLoadingIndicator = () => {
+    if (this.hasLoadingIndicatorTarget) {
+      this.loadingIndicatorTarget.classList.add("hidden");
+    }
+  };
 }
