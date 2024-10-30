d870cecd02911944acf2d4668f89bd1280899f19
Justin Marsh <jmarsh24@gmail.com>
2024-10-30 10:11:31 +0100

    enforce modal

---
+++ app/controllers/concerns/turbo/force_frame_response.rb
@@ -0,0 +1,17 @@
+module Turbo
+  module ForceFrameResponse
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def force_frame_response(options = {})
+        before_action :force_frame_response, **options
+      end
+    end
+
+    def force_frame_response
+      return if turbo_frame_request?
+
+      redirect_to(request.referer || root_path)
+    end
+  end
+end
