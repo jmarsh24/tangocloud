4fa8bf7f8605408e0aac42b051b8058690fe12b9
Justin Marsh <jmarsh24@gmail.com>
2024-10-31 19:00:15 +0100

    allow player to function across screens

--- app/javascript/controllers/music_player_controller.js
+++ app/javascript/controllers/music_player_controller.js
@@ -4,14 +4,11 @@ import WaveSurfer from "wavesurfer.js";
 export default class extends Controller {
   static targets = [
     "container",
-    "playButton",
-    "markerDescription",
     "time",
     "duration",
     "playIcon",
     "pauseIcon",
     "hover",
-    "loadingIndicator",
   ];
 
   static values = {
@@ -20,15 +17,13 @@ export default class extends Controller {
   };
 
   connect() {
-    if (!this.hasAudioUrlValue) return;
+    if (this.wavesurfer) {
+      return;
+    }
 
     this.createGradients();
     this.initializeWaveSurfer();
     this.setupEventListeners();
-  }
-
-  disconnect() {
-    this.wavesurfer?.destroy();
   }
 
   playPause() {
@@ -38,6 +33,13 @@ export default class extends Controller {
   handleHover = (e) => {
     if (this.hasHoverTarget) {
       this.hoverTarget.style.width = `${e.offsetX}px`;
+      this.hoverTarget.classList.remove("hidden");
+    }
+  };
+
+  hideHover = () => {
+    if (this.hasHoverTarget) {
+      this.hoverTarget.classList.add("hidden");
     }
   };
 
@@ -94,8 +96,6 @@ export default class extends Controller {
     this.wavesurfer.on("pause", this.onPause);
     this.wavesurfer.on("finish", this.onFinish);
     this.wavesurfer.on("decode", this.onDecode);
-    this.wavesurfer.on("ready", this.hideLoadingIndicator);
-    this.wavesurfer.on("audioprocess", this.showLoadingIndicator);
     this.wavesurfer.on("timeupdate", this.onTimeUpdate);
   }
 
@@ -117,17 +117,5 @@ export default class extends Controller {
 
   onTimeUpdate = (currentTime) => {
     this.timeTarget.textContent = this.formatTime(currentTime);
-  };
-
-  showLoadingIndicator = () => {
-    if (this.hasLoadingIndicatorTarget) {
-      this.loadingIndicatorTarget.classList.remove("hidden");
-    }
-  };
-
-  hideLoadingIndicator = () => {
-    if (this.hasLoadingIndicatorTarget) {
-      this.loadingIndicatorTarget.classList.add("hidden");
-    }
   };
 }
