ae909ad14e82aa52dbcabc844f95c0a9302f5620
Justin Marsh <jmarsh24@gmail.com>
2024-10-31 17:09:44 +0100

    add random items if queue is empty

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -4,21 +4,23 @@ class QueuesController < ApplicationController
   force_frame_response :show
 
   def show
-    @queue = PlaybackQueue.new(user: current_user)
+    @queue = policy_scope(PlaybackQueue).find_or_create_by(user: current_user)
 
     authorize @queue
 
-    recordings = policy_scope(Recording).limit(10)
-    tandas = policy_scope(Tanda).limit(10)
+    if @queue.queue_items.empty?
+      recordings = policy_scope(Recording).limit(10)
+      tandas = policy_scope(Tanda).limit(10)
 
-    recordings.each do |recording|
-      @queue.queue_items.build(item: recording)
+      recordings.each do |recording|
+        @queue.queue_items.build(item: recording)
+      end
+
+      tandas.each do |tanda|
+        @queue.queue_items.build(item: tanda)
+      end
+
+      @queue.save!
     end
-
-    tandas.each do |tanda|
-      @queue.queue_items.build(item: tanda)
-    end
-
-    @queue.save!
   end
 end
