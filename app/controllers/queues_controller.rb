12d7d1d1301659e1c6e2b035d19dbb2298e88fb0
Justin Marsh <jmarsh24@gmail.com>
2024-11-04 12:06:36 +0100

    update queues controller

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -2,7 +2,7 @@ class QueuesController < ApplicationController
   include RemoteModal
   before_action :set_queue
   before_action :set_playback_session
-  skip_after_action :verify_authorized, only: [:show, :next, :previous, :play_recording]
+  skip_after_action :verify_authorized, only: [:show, :next, :previous]
 
   def show
     authorize @playback_queue
@@ -11,20 +11,6 @@ class QueuesController < ApplicationController
     @playback_queue.ensure_default_items
 
     @playback_queue_items = @playback_queue.queue_items.including_item_associations.rank(:row_order).offset(1)
-  end
-
-  def play_recording
-    @recording = policy_scope(Recording).find(params[:recording_id])
-    authorize @recording, :play?
-
-    @playback_queue.play_recording(@recording)
-
-    @playback_queue_items = @playback_queue.queue_items.including_item_associations.rank(:row_order).offset(1)
-
-    render turbo_stream: [
-      turbo_stream.update("music-player", partial: "shared/music_player", locals: {playback_queue:, playback_session:}),
-      turbo_stream.update("queue", partial: "queues/queue", locals: {playback_queue:, playback_session:, queue_items:})
-    ]
   end
 
   def next
