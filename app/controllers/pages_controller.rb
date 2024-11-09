c03a83e4b8f76db017a340e34bad1cd9a6f450e3
Justin Marsh <jmarsh24@gmail.com>
2024-11-09 20:24:58 +0100

    fix redirect for new users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def landing
-    redirect_to music_library_path if Current.user.present?
+    redirect_to music_library_path if Current.user.tester? || Current.user.admin? || Current.user.editor?
   end
 
   def privacy
