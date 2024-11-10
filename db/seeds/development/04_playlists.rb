d31ee1b1ed9126a27a197f80ea84aee60de00c93
Justin Marsh <jmarsh24@gmail.com>
2024-11-10 11:32:53 +0100

    update production

--- db/seeds/development/04_playlists.rb
+++ db/seeds/development/04_playlists.rb
@@ -57,6 +57,6 @@ def attach_playlist_image(playlist, image_path)
   )
 end
 
-normal_user = User.find_by(email: "user@tangocloud.app")
+normal_user = User.find_by(email: Rails.application.credentials.dig(:el_recodo_email))
 
 create_playlists_for_user(normal_user) if normal_user
