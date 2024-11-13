5ca8eeb76c104f0b260a4548464577953610028c
Justin Marsh <jmarsh24@gmail.com>
2024-11-13 21:28:14 +0100

    add queue item policy

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -1,7 +1,10 @@
 class QueueItemsController < ApplicationController
   def reorder
     queue_item = @playback_queue.queue_items.find(params[:id])
-    queue_item.update(row_order_position: params[:position])
+    
+    authorize queue_item
+
+    queue_item.update!(row_order_position: params[:position])
     head :ok
   end
 end
