a167afc9db2acd20c5355e1da04394827588d7d7
Justin Marsh <jmarsh24@gmail.com>
2024-11-18 10:13:49 +0100

    fix time/duration

--- app/javascript/player.js
+++ app/javascript/player.js
@@ -52,9 +52,9 @@ export default class Player {
     });
 
     this.wavesurfer.on("timeupdate", (currentTime) => {
-      if (this.isReady) {
+      // if (this.isReady) {
         this.dispatchProgressEvent(currentTime);
-      }
+      // }
     });
 
     this.wavesurfer.on("finish", () => {
