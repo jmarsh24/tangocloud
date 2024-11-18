92150c2c6999e64235946d70634ddd9ab9ddae5b
Justin Marsh <jmarsh24@gmail.com>
2024-11-18 18:40:34 +0100

    fix bugs

--- app/controllers/searches/recordings_controller.rb
+++ app/controllers/searches/recordings_controller.rb
@@ -8,15 +8,28 @@ class Searches::RecordingsController < ApplicationController
     playback_queue = PlaybackQueue.find_or_create_by(user: current_user)
     playback_session = PlaybackSession.find_or_create_by(user: current_user)
 
-    playback_queue.play_recording(recording)
-
-    playback_session.play(reset_position: true)
-
     queue_items = playback_queue.queue_items.including_item_associations.rank(:row_order).offset(1)
 
-    render turbo_stream: [
-      turbo_stream.update("music-player", partial: "shared/music_player", locals: {playback_queue:, playback_session:}, method: "morph"),
-      turbo_stream.update("sidebar-queue", partial: "queues/queue", locals: {playback_queue:, playback_session:, queue_items:}, method: "morph")
-    ]
+    if playback_queue.current_item&.item == recording
+      playback_session.play(reset_position: false)
+
+      respond_to do |format|
+        format.turbo_stream { head :ok }
+      end
+    else
+      playback_queue.play_recording(recording)
+      playback_session.play(reset_position: true)
+
+      queue_items = playback_queue.queue_items.including_item_associations.rank(:row_order).offset(1)
+
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update("music-player", partial: "shared/music_player", locals: {playback_queue:, playback_session:}, method: "morph"),
+            turbo_stream.update("sidebar-queue", partial: "queues/queue", locals: {playback_queue:, playback_session:, queue_items:}, method: "morph")
+          ]
+        end
+      end
+    end
   end
 end
