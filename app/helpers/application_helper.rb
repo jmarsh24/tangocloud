c0a727645c8ad70990d3a72b0cae83c6ae241431
Justin Marsh <jmarsh24@gmail.com>
2024-11-21 23:34:04 +0100

    try it out!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def genre_color(genre_name)
+    case genre_name.downcase
+    when "tango"
+      "#111111"
+    when "milonga"
+      "#2A9D8F" # Teal for Milonga
+    when "vals"
+      "#E9C46A" # Orangish Yellow for Vals
+    else
+      "#6C757D" # Neutral Gray as a fallback
+    end
+  end
 end
