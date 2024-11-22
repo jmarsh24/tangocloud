1a23a6cb7996c67e2ab0529b466b917a0f8bf1aa
Justin Marsh <jmarsh24@gmail.com>
2024-11-22 17:17:15 +0100

    get the center

--- app/models/audio_recognition/audio_preprocessor.rb
+++ app/models/audio_recognition/audio_preprocessor.rb
@@ -3,7 +3,7 @@ class AudioPreprocessor
     Tempfile.create(["processed_audio", ".mp3"]) do |output_file|
       FFMPEG::Movie.new(audio_file.path).transcode(
         output_file.path,
-        audio_transcoding_options
+        audio_transcoding_options(audio_file)
       )
 
       yield output_file if block_given?
@@ -15,13 +15,18 @@ class AudioPreprocessor
 
   private
 
-  def audio_transcoding_options
+  def audio_transcoding_options(audio_file)
     {
       audio_codec: "libmp3lame", # Use MP3 codec
       audio_bitrate: "128k",     # Adjust bitrate if needed
       audio_sample_rate: 44100,  # Sample rate
-      seek_time: 0,              # Start trimming from 0 seconds
-      duration: 20               # Trim to 20 seconds
+      seek_time: center_of_audio(audio_file), # Start from the center
+      duration: 15               # Trim to 10 seconds
     }
+  end
+
+  def center_of_audio(audio_file)
+    movie = FFMPEG::Movie.new(audio_file.path)
+    movie.duration / 2
   end
 end
