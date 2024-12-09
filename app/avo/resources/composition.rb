8f3e73af6807168050adb0cbcfc268dba86a31f2
Justin Marsh <jmarsh24@gmail.com>
2024-12-09 23:09:49 +0100

    move to top

--- app/avo/resources/composition.rb
+++ app/avo/resources/composition.rb
@@ -7,12 +7,12 @@ class Avo::Resources::Composition < Avo::BaseResource
   def fields
     field :id, as: :id, readonly: true, only_on: :show
     field :title, as: :text
+    field :recordings, as: :has_many
+    field :digital_remasters, as: :has_many
     field :composers, as: :has_many
     field :lyricists, as: :has_many
-    field :recordings, as: :has_many
     field :composition_lyrics, as: :has_many
     field :lyrics, as: :has_many
     field :composition_roles, as: :has_many
-    field :digital_remasters, as: :has_many
   end
 end
